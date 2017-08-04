b7abd6c2157f10e492402d729a1f92f4032bb7a1
cchampou <clement@champouillon.com>
2017-08-04 14:21:57 +0200

    Minimal Router

--- src/App.js
+++ src/App.js
@@ -1,21 +1,38 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import ContributorSignup from './components/contributor/Signup';
+import CompanySignup from './components/company/Signup';
+import Home from './components/Home';
+
+import {
+    BrowserRouter as Router,
+    Route,
+    Link
+} from 'react-router-dom';
+
+
 import './App.css';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
+    render() {
+        return (
+            <Router>
+                <div>
+                    <div>
+                        <ul>
+                            <li><Link to="/">Accueil</Link></li>
+                            <li><Link to="/contributor/signup">Inscription AA</Link></li>
+                            <li><Link to="/company/signup">Inscription Entreprise</Link></li>
+                        </ul>
+                    </div>
+                    <div>
+                        <Route exact path="/" component={Home} />
+                        <Route exact path="/contributor/signup" component={ContributorSignup} />
+                        <Route exact path="/company/signup" component={CompanySignup} />
+                    </div>
+                </div>
+            </Router>
+        );
+    }
 }
 
 export default App;
