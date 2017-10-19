021fc831aefcb6ee514ada04d9335c9f1bdde1f8
cchampou <clement@champouillon.com>
2017-10-19 13:20:26 +0200

    Secured URL

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 
 
 module.exports = {
-    server_url : 'http://api.parrainage.confidencesdabeilles.fr',
+    server_url : 'https://api.parrainage.confidencesdabeilles.fr',
 	cdn_url : 'https://cdn.parrainage.confidencesdabeilles.fr'
 }
