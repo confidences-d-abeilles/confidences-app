b409415a60c49551dd735d6a9592906749f0acb2
cchampou <clement@champouillon.com>
2017-10-19 13:18:19 +0200

    Server and CDN url update

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 
 
 module.exports = {
-    server_url : 'http://localhost:1337',
-	cdn_url : 'https://dev586.confidencesdabeilles.fr/cdn'
+    server_url : 'http://api.parrainage.confidencesdabeilles.fr',
+	cdn_url : 'https://cdn.parrainage.confidencesdabeilles.fr'
 }
