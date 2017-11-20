2f99bdf5c2042830bdb74f4b781c0317fd01b363
cchampou <clement@champouillon.com>
2017-11-20 14:02:23 +0100

    Express commit

--- src/components/contributor/Parrains.js
+++ src/components/contributor/Parrains.js
@@ -26,7 +26,7 @@ export default class ContributorParrains extends Component {
 			<div className="container">
 				<NotificationSystem ref="notif" />
 				<h2 className="text-center my-2">Liste des entreprises déjà démarchées</h2>
-				<div className="row my-2">
+				<div className="row my-4 justify-content-center">
 					<div className="col-lg-6">
 						<input type="text" className="form-control" onChange={handleChange.bind(this)} name="filter" placeholder="Rechercher une raison sociale ou un numéro de SIRET" />
 					</div>
@@ -36,7 +36,7 @@ export default class ContributorParrains extends Component {
 						<tr><th>Raison sociale</th><th>Numéro de Siret</th></tr>
 						{this.state.leads.map((lead) => {
 							if (this.state.filter) {
-								if (lead.siret.indexOf(this.state.filter) >= 0 || lead.company_name.indexOf(this.state.filter) >= 0) {
+								if (lead.siret.toLowerCase().indexOf(this.state.filter.toLowerCase()) >= 0 || lead.company_name.toLowerCase().indexOf(this.state.filter.toLowerCase()) >= 0) {
 									return (
 										<tr key={lead.id}><td>{lead.company_name}</td><td>{lead.siret}</td></tr>
 									)
