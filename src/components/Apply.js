195f68c970c1018337c9000bf76107a70e85f056
cchampou <clement@champouillon.com>
2017-12-05 14:44:20 +0100

    Application form

--- src/components/Apply.js
+++ src/components/Apply.js
@@ -14,15 +14,29 @@ export default class Apply extends Component {
             phone: '',
             linkedin: '',
             presentation: '',
+            type: '1',
+            cv: '',
             success: false
         }
     }
 
     apply(e) {
         e.preventDefault();
+        const dataset = new FormData();
+        dataset.append('name', this.state.name);
+        dataset.append('firstname', this.state.firstname);
+        dataset.append('email', this.state.email);
+        dataset.append('phone', this.state.phone);
+        dataset.append('linkedin', this.state.linkedin);
+        dataset.append('presentation', this.state.presentation);
+        dataset.append('type', this.state.type);
+        if (document.getElementById("cv").files[0]) {
+            dataset.append('cv', document.getElementById("cv").files[0]);
+        }
         request({
             url: '/contact/apply',
-            method: 'post'
+            method: 'post',
+            data: dataset
         }, this.refs.notif).then((res) => {
             this.setState({
                 name: '',
@@ -31,6 +45,8 @@ export default class Apply extends Component {
                 phone: '',
                 linkedin: '',
                 presentation: '',
+                type: '1',
+                cv: '',
                 success: true
             })
         })
@@ -62,6 +78,32 @@ export default class Apply extends Component {
                             <div className="form-group">
                                 <input type="text" value={this.state.linkedin} onChange={handleChange.bind(this)} placeholder="Lien vers votre compte LinkedIn" name="linkedin" className="form-control"/>
                             </div>
+                            <div className="form-group">
+        						<label>Joindre votre CV</label>
+        						<label htmlFor="cv" className={(this.state.cv)?'active-upload':'upload'} style={{ position: 'relative' }}>
+        							<input type="file" className="form-control" id="cv" onChange={() => { this.setState({ cv : document.getElementById("cv").files[0].name }) }} style={{ position: 'absolute', height: '5.5em', top: '0', left: "0", opacity: '0.0001'}}/>
+        							Glisser votre CV ici ou cliquez pour en séléctionner un parmi vos fichers<br/>
+                                {(this.state.cv)?'Selectionné : '+this.state.cv:"Aucun fichier séléctionné"}
+        						</label>
+        					</div>
+                            <fieldset className="form-group">
+                                <legend>Type d'embauche</legend>
+                                <div className="form-check">
+                                    <label className="form-check-label">
+                                        <input type="radio" name="type" className="form-check-input" onChange={handleChange.bind(this)} checked={(this.state.type === '1')?true:false} value="1" /> Stage en communication / commercialisation
+                                        </label>
+                                    </div>
+                                    <div className="form-check">
+                                        <label className="form-check-label">
+                                            <input type="radio" name="type" className="form-check-input" onChange={handleChange.bind(this)} checked={(this.state.type === '2')?true:false} value="2" /> Stage en community management / business development
+                                            </label>
+                                        </div>
+                                        <div className="form-check">
+                                            <label className="form-check-label">
+                                                <input type="radio" name="type" className="form-check-input" onChange={handleChange.bind(this)} checked={(this.state.type === '3')?true:false} value="3" /> Candidature spontanée
+                                                </label>
+                                            </div>
+                                        </fieldset>
                             <div className="form-group">
                                 <textarea value={this.state.presentation} rows="10" onChange={handleChange.bind(this)} placeholder="C'est à vous. Présentez-vous rapidement et donnez nous envie de vous rappeler ! *" name="presentation" className="form-control"/>
                             </div>
