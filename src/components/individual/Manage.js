3f6bf71638c24cf676b4dee729fe03b5777b59f8
cchampou <clement@champouillon.com>
2017-12-08 15:57:41 +0100

    Auth

--- src/components/individual/Manage.js
+++ src/components/individual/Manage.js
@@ -6,6 +6,7 @@ import { Redirect, Route, Link } from 'react-router-dom';
 import IndividualManageInfos from './manage/Infos'
 import Bills from './manage/Bills'
 import NotificationSystem from 'react-notification-system'
+import Account from './manage/Account'
 
 export default class IndividualManage extends Component {
 
@@ -54,11 +55,10 @@ export default class IndividualManage extends Component {
 				{(!isLoggedIn)?<Redirect to="/" />:null}
 				<div className="row">
 					<div className="col-3">
-						<img src={imgPlaceholder} alt="Logo" className="img-fluid img-thumbnail" />
-						<br /><br />
 						<ul className="list-group">
 							<li className="list-group-item"><Link to="/individual/manage/infos">Mes informations</Link></li>
 							<li className="list-group-item"><Link to="/individual/manage/bills">Mes factures</Link></li>
+							<li className="list-group-item"><Link to="/individual/manage/account">Mon compte</Link></li>
 							<li className="list-group-item">Deconnexion</li>
 						</ul>
 					</div>
@@ -70,6 +70,7 @@ export default class IndividualManage extends Component {
 						</div>
 						<Route exact path="/individual/manage/bills" component={Bills} />
 						<Route exact path="/individual/manage/infos" component={IndividualManageInfos} />
+						<Route exact path="/individual/manage/account" component={Account} />
 					</div>
 				</div>
 			</div>
