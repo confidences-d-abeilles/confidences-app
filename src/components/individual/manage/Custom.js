0dda97881c20abfd29a07b414efd7a8f7ce5113d
cchampou <clement@champouillon.com>
2017-12-24 18:55:38 +0100

    Etiqu

--- src/components/individual/manage/Custom.js
+++ src/components/individual/manage/Custom.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react';
+import Version1 from '../../../assets/img/P/etiqu1.jpg';
+import Version2 from '../../../assets/img/P/etiqu2.jpg';
 
 export default class Custom extends Component {
 
@@ -12,9 +14,11 @@ export default class Custom extends Component {
     render () {
         return (
             <div className="row">
-                <div className="col-lg-12">
+                <div className="col-lg-12 text-center">
                     <h2 className="text-center my-5">Choix des étiquettes</h2>
-                    <p className="alert alert-info">Cette fonctionnalité est en cours de développement et sera disponible d'ici quelques jours. Merci de votre patience.</p>
+                    <p className="alert alert-info">Cette fonctionnalité est en cours de développement et sera disponible d'ici quelques jours. Merci de votre patience. En attendant, voici un aperçu des futures étiquettes ;)</p>
+                    <img className="img-fluid" src={Version1} alt="Modèle 1"/><hr />
+                    <img className="img-fluid" src={Version2} alt="Modèle 2"/>
                 </div>
             </div>
         )
