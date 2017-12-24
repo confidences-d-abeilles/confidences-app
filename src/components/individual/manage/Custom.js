f3ccfb3269302dc33063f679d4ecb99b12c314b3
cchampou <clement@champouillon.com>
2017-12-24 17:06:06 +0100

    Custom

--- src/components/individual/manage/Custom.js
+++ src/components/individual/manage/Custom.js
@@ -14,6 +14,7 @@ export default class Custom extends Component {
             <div className="row">
                 <div className="col-lg-12">
                     <h2 className="text-center my-5">Choix des étiquettes</h2>
+                    <p className="alert alert-info">Cette fonctionnalité est en cours de développement et sera disponible d'ici quelques jours. Merci de votre patience.</p>
                 </div>
             </div>
         )
