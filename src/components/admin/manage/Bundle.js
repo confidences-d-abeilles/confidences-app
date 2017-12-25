9a883f6a783726e6f9cdc9061cfd9ed0a215eecc
cchampou <clement@champouillon.com>
2017-12-25 01:54:08 +0100

    phone

--- src/components/admin/manage/Bundle.js
+++ src/components/admin/manage/Bundle.js
@@ -40,8 +40,7 @@ export default class Bundle extends Component {
 				stateSelector: res.state,
 				present_name: res.name,
 				present_firstname: res.firstname,
-				present_email: res.email,
-				present_phone: res.phone
+				present_email: res.email
 			})
 		})
 
@@ -260,7 +259,6 @@ export default class Bundle extends Component {
 												<input type="text" name="present_email" onChange={handleChange.bind(this)} value={this.state.present_email} placeholder="Email" className="form-control" />
 											</div>
 											<button className="btn btn-secondary btn-sm my-2" >Valider</button>
-											Téléphone de livraison : {this.state.present_phone}
 										</form>
 										:'Ce parrainage n\'est pas un cadeau'}
 									</p>
