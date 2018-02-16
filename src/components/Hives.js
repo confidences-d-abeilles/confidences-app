9cdb534088a6004dbeacc5d889b2d58be5bb28c4
Clement CHAMPOUILLON <clement@champouillon.com>
2018-02-16 15:54:38 +0100

    Les ruches

--- src/components/Hives.js
+++ src/components/Hives.js
@@ -15,7 +15,7 @@ export default class Hives extends Component {
 	constructor(props) {
 		super(props)
 		this.state = {
-			bundles : null,
+			hives : null,
 			criteria: ''
 		}
 		ReactGA.pageview(this.props.location.pathname);
@@ -23,11 +23,11 @@ export default class Hives extends Component {
 
 	componentDidMount() {
 		request({
-			url: '/bundle',
+			url: '/hive',
 			method: 'get'
 		}, this.refs.notif).then((res) => {
 			this.setState({
-				bundles : res
+				hives : res
 			})
 		})
 	}
@@ -40,25 +40,16 @@ export default class Hives extends Component {
 				<h1 style={{ fontFamily: "HighTo", color: '#E49C00' }} className="text-center my-5">LES RUCHES</h1>
 				<div className="row justify-content-center">
 					<div className="col">
-						{this.state.bundles?
+						{this.state.hives?
 						<div className="row justify-content-center">
-							{this.state.bundles.map((bundle) => {
-								const owner = (bundle.owner.company_name)?bundle.owner.company_name:bundle.owner.firstname+' '+bundle.owner.name;
-								const datetime = new Date(bundle.createdAt);
-								return (
-									bundle.contain.map((hive) => {
-										return (
-											<div className="card w-25 m-3 justify-content-between">
-												<img className="card-img-top img-fluid" src={(hive.imgs && hive.imgs[0])?config.cdn_url+'/'+hive.imgs[0]:imgPlaceholder} alt="Card image cap" />
-												<div className="card-block" style={{ height: 'auto', flex: '0' }}>
-													<h3 className="card-title">{hive.name}</h3>
-													<h6 className="card-subtitle text-muted">Parrainée par {owner} depuis le {datetime.getDate()+'/'+parseInt(datetime.getMonth()+1)+'/'+datetime.getFullYear()}</h6>
-													<Link to={'/hive/'+hive.id} className="btn">Voir en détails</Link>
-												</div>
-											</div>
-										)
-									})
-								)
+							{this.state.hives.map((hive) => {
+								return (<div className="card w-25 m-3 justify-content-between">
+									<img className="card-img-top img-fluid" src={(hive.imgs && hive.imgs[0])?config.cdn_url+'/'+hive.imgs[0]:imgPlaceholder} alt="Card image cap" />
+									<div className="card-block" style={{ height: 'auto', flex: '0' }}>
+										<h3 className="card-title text-center" style={{ fontFamily: "HighTo"}} >{hive.name}</h3>
+										<Link to={'/hive/'+hive.id} className="btn btn-link float-right">Voir en détail</Link>
+									</div>
+								</div>)
 							})}
 						</div>:<Loading />}
 					</div>
