48691df1332af226f63f078de02af34245620de3
Clement CHAMPOUILLON <clement@champouillon.com>
2018-03-01 19:07:44 +0100

    Paiment update

---
+++ src/components/Pay.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import Loading from './utils/Loading';
+
+const config = require("../config.js");
+
+export default class Pay extends Component {
+
+	constructor(props) {
+		super(props);
+		this.io = require('../assets/js/sails.io.js')( require('socket.io-client') );
+		this.io.sails.url = config.server_url;
+	}
+
+	render () {
+		return (
+			<div>
+			</div>
+		)
+	}
+}
