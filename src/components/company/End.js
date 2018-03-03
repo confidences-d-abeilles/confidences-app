68ac3a680c7c188c337478bbc9875fd17bf78516
Clement CHAMPOUILLON <clement@champouillon.com>
2018-03-03 13:08:16 +0100

    COmpany End

--- src/components/company/End.js
+++ src/components/company/End.js
@@ -4,18 +4,36 @@ import Main from '../../assets/img/end_part.jpg';
 import imgPlaceholder from '../../assets/img/img-placeholder.gif';
 import ReactGA from 'react-ga';
 import Meta from '../utils/Meta'
+import request from '../../services/Net';
+import NotificationSystem from 'react-notification-system'
 
 export default class CompanyEnd extends Component {
-
-    constructor(props) {
-        super(props)
-        ReactGA.pageview(this.props.location.pathname);
-    }
-
-    render () {
-        return (
+	
+	constructor(props) {
+		super(props)
+		ReactGA.pageview(this.props.location.pathname);
+		this.state = {
+			namespace: ''
+		}
+	}
+	
+	componentDidMount() {
+		request({
+			url : '/user/me',
+			method : 'get'
+		}, this.refs.notif)
+		.then((res) => {
+			this.setState({
+				namespace: res.namespace
+			})
+		});
+	}
+	
+	render () {
+		return (
 			<div className="container py-4">
-                <Meta title="Félicitations"/>
+				<Meta title="Félicitations"/>
+				<NotificationSystem ref="notif" />
 				<div className="row justify-content-center">
 					<div className="col-8">
 						<h2 className="text-center my-4">Félicitations ! Vous faites désormais partie de la grande famille des parrains de ruches.</h2>
@@ -29,12 +47,12 @@ export default class CompanyEnd extends Component {
 								<Link to="/company/manage" className="btn btn-primary btn-lg">Mon compte</Link>
 							</div>
 							<div className="col text-center">
-								<button className="btn btn-primary btn-lg">Découvrir notre page</button>
+								<Link to={'/parrains/'+this.state.namespace}  className="btn btn-primary btn-lg">Découvrir notre page</Link>
 							</div>
 						</div>
 					</div>
 				</div>
 			</div>
-        );
-    }
+		);
+	}
 }
