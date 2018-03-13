957409a32c1bb78552435df78281d8ef8f64be38
Clement CHAMPOUILLON <clement@champouillon.com>
2018-03-13 14:18:11 +0100

    News Page E

--- src/components/utils/FileUpload.js
+++ src/components/utils/FileUpload.js
@@ -6,6 +6,7 @@ export default class FileUpload extends Component {
  * Form component to upload a file
  * Provide a label props
  * Provide a identifier
+ * You can specify a accept attribute
  */
 
 	constructor(props) {
@@ -21,7 +22,7 @@ export default class FileUpload extends Component {
 			<div className="form-group">
 				<label>{this.props.label}</label>
 				<label htmlFor="logo" className={(this.state.newFile)?'active-upload':'upload'} style={{ position: 'relative' }}>
-					<input type="file" className="form-control" id={this.identifier} onChange={() => { this.setState({ newFile : document.getElementById(this.identifier).files[0].name }) }} style={{ position: 'absolute', height: '5.5em', top: '0', left: "0", opacity: '0.0001'}}/>
+					<input type="file" className="form-control" id={this.identifier} onChange={() => { this.setState({ newFile : document.getElementById(this.identifier).files[0].name }) }} style={{ position: 'absolute', height: '5.5em', top: '0', left: "0", opacity: '0.0001'}} accept={(this.props.accept)?this.props.accept:''} />
 					Glissez un fichier ici ou cliquez pour en séléctionner un<br/>
 					{(this.state.newFile)?'Selectionné : '+this.state.newFile:"Aucun fichier séléctionné"}
 				</label>
