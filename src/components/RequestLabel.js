e1a1b3cebdd32e34f09e46df0be52923b5b098f9
Sonic <kevgusma@student.42.fr>
2018-03-20 15:07:31 +0100

    request for custom label

---
+++ src/components/RequestLabel.js
@@ -0,0 +1,92 @@
+import React, { Component } from 'react'
+import { handleChange } from '../services/FormService'
+import request from '../services/Net'
+import NotificationSystem from 'react-notification-system'
+import ReactGA from 'react-ga';
+import Meta from './utils/Meta'
+
+export default class RequestLabel extends Component {
+
+  constructor(props) {
+      super(props);
+      ReactGA.pageview(this.props.location.pathname);
+      this.state = {
+          name: '',
+          firstname: '',
+          society: '',
+          email: '',
+          feedback: '',
+          attachment: '',
+
+          success: false
+      }
+  }
+
+  sendMail(e) {
+    e.preventDefault();
+    const data = new FormData();
+    data.append('name', this.state.name);
+    data.append('firstname', this.state.firstname);
+    data.append('society', this.state.society);
+    data.append('email', this.state.email);
+    data.append('feedback', this.state.feedback);
+    if (document.getElementById("attachment").files[0]) {
+        data.append('attachment', document.getElementById("attachment").files[0]);
+    }
+    request({
+      url: '/contact/label',
+      method: 'post',
+      data: data
+    }, this.refs.notif).then((res) => {
+      console.log("faire un setState a null");
+    })
+    console.log("coucou");
+  }
+
+   render(){
+     return (
+         <div className="container">
+             <Meta title="Label"/>
+             <NotificationSystem ref="notif" />
+             <div className="row justify-content-center">
+                 <div className="col-lg-6 col-md-10 col-sm-12">
+                     <h2 className="text-center my-4">
+                      <span className="align-middle">Demande de Label : descriptif ? </span>
+                      </h2>
+                     <form onSubmit={this.sendMail.bind(this)} className="mt-4">
+                         <div className="form-group">
+                             <input type="text" value={this.state.name} onChange={handleChange.bind(this)} placeholder="Votre nom *" name="name" className="form-control"/>
+                         </div>
+                         <div className="form-group">
+                             <input type="text" value={this.state.firstname} onChange={handleChange.bind(this)} placeholder="Votre prenom *" name="firstname" className="form-control"/>
+                         </div>
+                         <div className="form-group">
+                             <input type="text" value={this.state.society} onChange={handleChange.bind(this)} placeholder="Nom de l'entreprise *" name="society" className="form-control"/>
+                         </div>
+                         <div className="form-group">
+                             <input type="text" value={this.state.email} onChange={handleChange.bind(this)} placeholder="Votre email *" name="email" className="form-control"/>
+                         </div>
+                         <div className="form-group">
+                          <label>Joindre un fichier (png, jpg, pdf, indd, ai)</label>
+                            <label htmlFor="attachment" className={(this.state.attachment)?'active-upload':'upload'} style={{ position: 'relative' }}>
+                            <input type="file" className="form-control" id="attachment" onChange={() => { this.setState({ attachment: document.getElementById("attachment").files[0].name }) }} style={{ position: 'absolute', height: '5.5em', top: '0', left: "0", opacity: '0.0001'}}/>
+                              Glissez votre image ici ou cliquez pour en séléctionner un parmi vos fichiers<br/>
+                             {(this.state.attachment)?'Selectionné : '+this.state.attachment:"Aucun fichier séléctionné"}
+                            </label>
+                          </div>
+
+                         <div className="form-group">
+                             <textarea value={this.state.feedback} rows="10" onChange={handleChange.bind(this)} placeholder="description de la demande *" name="feedback" className="form-control"/>
+                         </div>
+                         {this.state.success &&
+                         <p className="alert alert-success">Votre demande a été envoyée avec succès</p>}
+                         <button className="btn btn-primary mb-4">Envoyer</button>
+                     </form>
+                 </div>
+             </div>
+         </div>
+     )
+
+   }
+
+}
