7f4ec880c163916113ca82ff65176d2f045b2754
Sonic <kevgusma@student.42.fr>
2018-03-28 17:35:59 +0200

    update view customize and requestlabel

--- src/components/company/manage/Customize.js
+++ src/components/company/manage/Customize.js
@@ -59,57 +59,90 @@ export default class CompanyManageCustomize extends Component {
 	}
 	}
 
+	// <form onSubmit={this.upload.bind(this)} className="my-2">
+	// 	<div className="form-group">
+	// 		<label htmlFor="label" className={(this.state.label)?'active-upload':'upload'}>Glisser votre fichier ici ou cliquez pour en séléctionner un parmi vos fichiers<br/>Taille recommandée : 280x210 - {(this.state.label)?'Selectionné : '+this.state.label:"Aucun fichier séléctionné"}</label>
+	// 		<input type="file" className="form-control" id="label" onChange={() => { this.setState({ label : document.getElementById("label").files[0].name }) }} style={{display:'none'}}/>
+	// 	</div>
+	// 	<button className="btn btn-primary">Envoyer cette étiquette</button>
+	// </form>
+
+
 	render () {
 		return (
+			<div>
 			<div className="row">
 				<NotificationSystem ref="notif" />
 				<div className="col">
-					<h2 className="text-center my-4">Personaliser nos pots de miel</h2>
+					<h2 className="text-center my-4"></h2>
 					{this.state.labelCurrent ?
 						<div style={{ height: '210px', maxWidth: '100%' }}>
 							<img src={config.cdn_url+'/'+this.state.labelCurrent} alt="labelCurrent" style={{ maxWidth: '100%', maxHeight: '100%' }} />
 						</div>
-					:null
-					}
-
-					{/* <p>
-						Vous avez la possibilité d’apporter les modifications que vous souhaitez sur
-						l’étiquette qui sera apposée sur vos pots de miel. Pour cela, il vous suffit de
-						la télécharger ci-dessous (format AI, Indd), de l’éditer avec Adobe Illustrator,
-						InDesign ou Photoshop ; une fois le résultat final obtenu vous nous renvoyez
-						votre fichier (format AI, Indd, Psd ou PDF).<br />
-						Nous attirons votre attention sur le fait que les textes cadenassés ne sont
-						pas modifiables. Ce sont des mentions obligatoires qui doivent figurer sur les
-						pots. Nous nous réservons le droit d’augmenter le contraste si celui-ci venait
-						à être diminué en raison d’un fond sombre.
-					</p> */}
-					<form onSubmit={this.upload.bind(this)} className="my-2">
-						<div className="form-group">
-							<label htmlFor="label" className={(this.state.label)?'active-upload':'upload'}>Glisser votre fichier ici ou cliquez pour en séléctionner un parmi vos fichiers<br/>Taille recommandée : 280x210 - {(this.state.label)?'Selectionné : '+this.state.label:"Aucun fichier séléctionné"}</label>
-							<input type="file" className="form-control" id="label" onChange={() => { this.setState({ label : document.getElementById("label").files[0].name }) }} style={{display:'none'}}/>
-						</div>
-						<button className="btn btn-primary">Envoyer cette étiquette</button>
-					</form>
-
-					<Link to="/requestlabel" className="btn btn-primary">
-						Faire appel aux services de notre graphiste
-					</Link>
-					<p>
-						Les fonctionnalités d'édition de vos étiquettes sont en cours de développement
-						et vont rapidement voir le jour ! <br />
-						Nous vous préviendrons lorsque ce sera le cas.
-					</p>
-					{/*<a href="#" target="_blank" className="btn btn-primary">Télécharger l'étiquette type</a>
-					<form onSubmit={this.upload.bind(this)} className="my-2">
-						<div className="form-group">
-							<label htmlFor="label" className={(this.state.label)?'active-upload':'upload'}>Glisser votre fichier ici ou cliquez pour en séléctionner un parmi vos fichiers<br/>Taille recommandée : 280x210 - {(this.state.label)?'Selectionné : '+this.state.label:"Aucun fichier séléctionné"}</label>
-							<input type="file" className="form-control" id="label" onChange={() => { this.setState({ label : document.getElementById("label").files[0].name }) }} style={{display:'none'}}/>
-						</div>
-						<button className="btn btn-primary">Envoyer cette étiquette</button>
-					</form>
-				*/}
+					:null}
 				</div>
 			</div>
+			<div className="row">
+				<div className="col-5">
+					<div>
+						<h5 className="text-center my-4">Notre étiquette personnalisée</h5>
+						<i style={{fontSize: '75%'}}>Une version « par défaut » est générée automatiquement. 3
+						solutions sont à votre disposition pour la modifier.</i>
+						<div style={{ width: '30%', height: '30%', backgroundColor: '#84B183', color: 'white', lineHeight: '3em', textAlign: 'center', bordel: '5px solid black', borderWith: '25px', padding: '10px'}}>
+						Miel Apple
+						</div>
+					</div>
+					<div>
+						<h5 className="text-center my-4">Réalisation en interne</h5>
+						<p style={{fontSize: '75%'}}>Téléchargez un modèle (format AI, INDD),
+						Modifiez-le,
+						Renvoyez-nous votre version !</p>
+						<button className="btn btn-primary">
+							Télécharger le modèle
+						</button>
+						<form onSubmit={this.upload.bind(this)} className="my-2">
+							<div className="form-group">
+								<div style={{fontSize: '80%'}}><label htmlFor="label" className={(this.state.label)?'active-upload':'upload'}>Glissez votre fichier ou cliquez pour en sélectionner un
+								(formats acceptés : PDF, AI, INDD, PSD, PNG, JPG)<br/>
+								⚠ATTENTION ⚠Assurez-vous que les dimensions de votre
+								étiquette respectent bien 63,5x38,1mm, avec 1,5mm de fond
+								perdu gauche/droite et 0,5mm haut/bas. -
+								{(this.state.label)?'Selectionné : '+this.state.label:"Aucun fichier séléctionné"}</label>
+								</div>
+								<input type="file" className="form-control" id="label" onChange={() => { this.setState({ label : document.getElementById("label").files[0].name }) }} style={{display:'none'}}/>
+							</div>
+							<button className="btn btn-primary">Soumettre cette étiquette</button>
+						</form>
+					</div>
+				</div>
+					<div className="col-6">
+
+					<h5 className="text-center my-4">Éditeur en ligne</h5>
+						<button className="btn btn-primary">Démarrer ?</button>
+						Templates disponibles
+						<div className="row">
+							<div className="col-4" style={{ width: '100px', height: '100px', backgroundColor: '#F4B183', color: 'white', lineHeight: '3em', textAlign: 'center', bordel: 'solid grey 1 px', padding: '10px'}}>
+							Miel Apple
+							</div>
+							<div className="col-4" style={{ width: '30%', height: '30%', backgroundColor: '#F4B183', color: 'white', lineHeight: '3em', textAlign: 'center', bordel: 'solid grey 1 px', padding: '10px'}}>
+							Miel Apple
+							</div>
+						</div>
+						<div className="row">
+							<div className="col-4" style={{ width: '30%', height: '30%', backgroundColor: '#F4B183', color: 'white', lineHeight: '3em', textAlign: 'center', bordel: 'solid grey 1 px', padding: '10px'}}>
+							Miel Apple
+							</div>
+							<div className="col-4" style={{ width: '30%', height: '30%', backgroundColor: '#F4B183', color: 'white', lineHeight: '3em', textAlign: 'center', bordel: 'solid grey 1 px', padding: '10px'}}>
+							Miel Apple
+							</div>
+						</div>
+						<Link to="/requestlabel" className="btn btn-primary">
+							Contacter une graphiste
+						</Link>
+					
+				</div>
+			</div>
+		</div>
 		)
 	}
 }
