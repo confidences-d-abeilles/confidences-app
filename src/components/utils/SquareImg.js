785da6679ea54b81b9e8014e8a622dfc315cef13
Sonic <kevgusma@student.42.fr>
2018-03-30 13:22:51 +0200

    img hive

--- src/components/utils/SquareImg.js
+++ src/components/utils/SquareImg.js
@@ -8,7 +8,7 @@ export default class SquareImg extends Component {
 
 	render () {
 		return (
-			<div style={{ width: '100%', paddingTop: '100%', backgroundImage: `url(${this.props.src})`, backgroundSize: 'cover' }}>
+			<div style={{ width: '80%', paddingTop: '100%', backgroundImage: `url(${this.props.src})`, backgroundSize: 'cover' }}>
 			</div>
 		)
 	}
