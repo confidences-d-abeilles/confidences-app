94eb14acc390c5d01d0369ee83bb752099e12e7a
Clement CHAMPOUILLON <clement@champouillon.com>
2018-05-12 01:34:55 +0200

    Meta update

--- public/meta.js
+++ public/meta.js
@@ -20,7 +20,10 @@ module.exports = {
                 }
             default:
                 return {
-                    title: "Confidences d'Abeilles"
+                    title: "Confidences d'Abeilles",
+                    ogtitle: "Parrainez des abeilles et soutenez-nous !",
+                    ogdescription: "Le parrainage est une action commune. Ensemble, nous agissons en faveur de la filière française du miel et, plus largement, nous protégeons nos chères butineuses et notre environnement.",
+                    ogurl: "https://parrainagederuches.fr/"
                 }
         }
     }
