327667d47b3a8d90e45bde6592e93b3f3360d436
Clement CHAMPOUILLON <clement@champouillon.com>
2018-05-12 01:13:31 +0200

    Tags update

--- public/meta.js
+++ public/meta.js
@@ -11,6 +11,13 @@ module.exports = {
                 return {
                     title: "L'équipe"
                 }
+            case '/jobs':
+                return {
+                    title: "Jobs | Confidences d'Abeilles",
+                    ogtitle: "Vous cherchez un stage ? C’est parfait, on embauche !",
+                    ogdescription: "Rejoindre une jeune équipe, dynamique, passionnée et participer au succès de son aventure vous tente ? Formidable, nous cherchons un(e) developer ReactJS, un(e) UX designer, un(e) communication | marketing manager",
+                    ogurl: "https://parrainagederuches.fr/jobs"
+                }
             default:
                 return {
                     title: "Confidences d'Abeilles"
--- server.js
+++ server.js
@@ -20,6 +20,20 @@ app.listen(5000, () => {
 
 
 function composeHtml(html, meta) {
-    html = html.replace("<title>Confidences d'Abeilles</title>", "<title>"+meta.title+"</title>");
+    if (meta.title) {
+        html = html.replace("<title>Confidences d'Abeilles</title>", "<title>"+meta.title+"</title>");
+    }
+    if (meta.ogtitle) {
+        html = html.replace('<meta id="og-title" property="og:title" content="Parrainez des abeilles et soutenez-nous !" />',
+        '<meta id="og-title" property="og:title" content="'+meta.ogtitle+'" />');
+    }
+    if (meta.ogdescription) {
+        html = html.replace('<meta id="og-description" property="og:description" content="Le parrainage est une action commune. Ensemble, nous agissons en faveur de la filière française du miel et, plus largement, nous protégeons nos chères butineuses et notre environnement." />',
+        '<meta id="og-description" property="og:description" content="'+meta.ogdescription+'" />');
+    }
+    if (meta.ogurl) {
+        html = html.replace('<meta id="og-url" property="og:url" content="https://parrainagederuches.fr" />',
+        '<meta id="og-url" property="og:url" content="'+meta.ogurl+'" />');
+    }
     return html;
 }
