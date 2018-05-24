63cfc875426d59d687d3e8ebbf56a1dee4332d97
Clement CHAMPOUILLON <clement@champouillon.com>
2018-05-24 19:39:43 +0200

    New loading

--- src/components/utils/Loading.js
+++ src/components/utils/Loading.js
@@ -1,11 +1,11 @@
 import React, { Component } from 'react';
-import loader from '../../assets/img/load2.gif'
+import loader from '../../assets/img/three-dots.svg'
 
 export default class Loading extends Component {
 
 	render() {
 		return (
-			<div style={{ width: '100%', textAlign: 'center'}}>
+			<div style={{ width: '100%', textAlign: 'center', height: '60px', paddingTop: '20px' }}>
 				<img src={loader} alt="Loading" />
 			</div>
 		)
