4fbc66144de8a4f01172055b2f4ed629045e13e8
Clement CHAMPOUILLON <clement@champouillon.com>
2018-05-24 18:13:25 +0200

    Added confirmation before user deletion

--- src/components/admin/manage/users/tiles/General/General.js
+++ src/components/admin/manage/users/tiles/General/General.js
@@ -2,8 +2,9 @@ import React from 'react';
 import Loading from '../../../../../utils/Loading'
 import moment from 'moment'
 import UserType from '../../UserType/UserType'
+import Confirm from '../../../../../utils/Confirm'
 
-const Genral = ( props ) => (
+const General = ( props ) => (
 	<div className="newcard">
 		{(props.data)?
 			<div>
@@ -11,10 +12,10 @@ const Genral = ( props ) => (
 				<p className="text-muted m-0"><small>Inscrit le {moment(props.data.createdAt).format("DD/MM/YYYY HH:mm:ss")}</small></p>
 				<p className="m-0">Email : {props.data.email}</p>
 				<p className="m-0">Téléphone : {props.data.phone}</p>
-				<button className="btn btn-danger btn-sm mt-2" onClick={props.delete} >Supprimer cet utilisateur</button>
+				<Confirm text="Supprimer cet utilisateur" class="btn btn-danger btn-sm mt-2" action={props.delete} />
 			</div>
 		:<Loading />}
 	</div>
 );
 
-export default Genral;
+export default General;
