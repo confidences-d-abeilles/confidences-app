97c9ab0bf926c9261544653020ee0ce7ae283c2a
Clement CHAMPOUILLON <clement@champouillon.com>
2018-05-24 15:39:19 +0200

    Reprise page payment P

--- src/components/individual/Payment.js
+++ src/components/individual/Payment.js
@@ -12,12 +12,9 @@ export default class IndividualPayement extends Component {
 	constructor(props) {
 		super(props);
 		this.state = {
-			bees: null,
 			redirect: false
 		}
 		ReactGA.pageview(this.props.location.pathname);
-		this.transferBank = false;
-		this.transferBankDone = false;
 	}
 
 	componentDidMount() {
@@ -27,60 +24,28 @@ export default class IndividualPayement extends Component {
 		}, this.refs.notif)
 		.then((res) => {
 			this.setState({
-				bees: res.bundles[0].bees,
 				price: res.bundles[0].price,
 				bundle_id: res.bundles[0].id,
-				bundle_present: res.bundles[0].present,
-				start_date: res.bundles[0].start_date,
-				duplicate: true
 			})
 		})
 	}
 
-	setWaitingPayment = async transferDone => {
-		this.transferBank = true;
-		this.transferBankDone = transferDone;
-
-		this.save().then((res) => {
-			this.setState({ redirect : false })
-		});
-	}
-
-	async save() {
-		console.log('save bundle request');
+	updateBundleState = (state) => {
 		return new Promise(resolve => {
 			request({
 				url: '/bundle/'+this.state.bundle_id,
 				method: 'put',
 				data : {
-					state : this.transferBank?1:this.state.bundleState,
-					bankTransferDone: (this.transferBankDone?'true':'false'),
-					present_date: (this.state.present)?this.state.start_date:new Date(),
+					state : state
 				}
 			}, this.refs.notif).then((res) => {
-				resolve();
+				this.setState({ redirect : true })
 			})
 		});
 	}
 
-	// setWaitingPayment() {
-	// 	const data = new FormData();
-	// 	data.append('state', 1);
-	// 	if (this.state.present === false) {
-	// 		data.append('present_date', new Date());
-	// 		// data.append('present_end', new Date(new Date().setFullYear(new Date().getFullYear() + 1)));
-	// 	}
-	// 	request({
-	// 		url: '/bundle/'+this.state.bundle_id,
-	// 		method: 'put',
-	// 		data : data
-	// 	}, this.refs.notif).then((res) => {
-	// 		this.setState({ redirect : true })
-	// 	})
-	// }
-
-    render () {
-        return (
+	render () {
+		return (
 			<div className="container py-4">
 				<Meta title="Paiement"/>
 				<NotificationSystem ref="notif" />
@@ -92,7 +57,7 @@ export default class IndividualPayement extends Component {
 					<div className="col-lg-6">
 						<h3 className="text-center my-4"><small>Paiement sécurisé par carte bancaire</small></h3>
 						<Elements locale="fr">
-							<PayForm price={this.state.price} before={this.save.bind(this)} bundle={this.state.bundle_id} date={(this.state.present)?this.state.start_date:new Date()} for={this.state.firstname+' '+this.state.name} endpoint="/individual/end" />
+							<PayForm price={this.state.price} bundle={this.state.bundle_id} date={(this.state.present)?this.state.start_date:new Date()} for={this.state.firstname+' '+this.state.name} endpoint="/individual/end" />
 						</Elements>
 					</div>
 					<div className="col-lg-6" style={{ borderStyle: 'solid', borderColor: '#E49C00', borderWidth: '0 0 0 4px'}}>
@@ -111,12 +76,12 @@ export default class IndividualPayement extends Component {
 						De	notre	côté,	la	validation	de	votre	virement	sera	faite	sous	48h.
 						</p>
 					<p className="text-center">
-						<button onClick={e => this.setWaitingPayment(false, e)} className="btn btn-primary">Virement en cours</button>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-						<button onClick={e => this.setWaitingPayment(true, e)} className="btn btn-primary">Virement effectué</button>
+						<button onClick={this.updateBundleState.bind(this, 0)} className="btn btn-primary">Bénéficiaire ajouté</button>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+						<button onClick={this.updateBundleState.bind(this, 1)} className="btn btn-primary">Virement effectué</button>
 					</p>
 					</div>
 				</div>
 			</div>
-        );
-    }
+		);
+	}
 }
