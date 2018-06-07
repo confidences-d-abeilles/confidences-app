0bafcf95caa1b51ad753659735cdf6e932779e67
Clement CHAMPOUILLON <clement@champouillon.com>
2018-06-07 22:43:45 +0200

    Automail

--- src/components/admin/manage/Mails.js
+++ src/components/admin/manage/Mails.js
@@ -1,12 +1,22 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import request from '../../../services/Net';
+import NotificationSystem from 'react-notification-system';
 
 
 export default class AdminManageMails extends Component {
 
+	launch = () => {
+		request({
+			url : '/automailer',
+			method : 'POST'
+		});
+	}
+
 	render () {
 		return (
 			<div>
-				
+				<NotificationSystem ref="notif" />
+				<button className="btn btn-danger" onClick={this.launch} >Launch email</button>
 			</div>
 		)
 	}
