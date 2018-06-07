7e5d9197c7d79ee5b7cc6ce064dfa7c65e32fca1
Clement CHAMPOUILLON <clement@champouillon.com>
2018-06-07 22:56:03 +0200

    Fix memo en boucle

--- src/components/admin/manage/users/tiles/Memo/ViewMemo.js
+++ src/components/admin/manage/users/tiles/Memo/ViewMemo.js
@@ -3,7 +3,7 @@ import Loading from '../../../../../utils/Loading'
 
 const ViewMemo = props => (
 	<div>
-		{(props.content)?props.content:<Loading />}<br />
+		{(typeof props.content !== 'undefined')?props.content:<Loading />}<br />
 		<button className="btn btn-sm btn-secondary my-2" onClick={props.edit} >Editer</button>
 	</div>
 )
