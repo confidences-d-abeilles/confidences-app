0b4bb983043cbe26c76c7fd6ae3267c9bc6eecb1
Nicolas Ekszterowicz <nicolas.ekszterowicz@gmail.com>
2018-07-13 15:10:26 +0200

    update download label, set filename, add view label

--- src/components/admin/manage/bundle/tiles/Label.js
+++ src/components/admin/manage/bundle/tiles/Label.js
@@ -1,9 +1,16 @@
 import React from 'react'
+import SquareImg from '../../../../utils/SquareImg';
+const config = require('../../../../../config.js')
 
 const Label = ( props ) => (
-	<div className="mb-4 text-center">
+	<div className="mb-4 text-center">{props.labelUrl}
 		<h3 className="card-title text-left">Etiquette</h3>
 		<button className="btn btn-info btn-sm" onClick={props.downloadLabel}>Télécharger</button>
+		{props.labelFilename &&
+			<a href={config.cdn_url+'/label/'+props.labelFilename} target="_blank">
+				<SquareImg className="card-img-top img-fluid" src={config.cdn_url+'/label/'+props.labelFilename} alt="Bundle label" />
+			</a>
+		}
 	</div>
 )
 
