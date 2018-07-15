8353f991e7c912c0cc93d378bdff52735459cece
Nicolas Ekszterowicz <nicolas.ekszterowicz@gmail.com>
2018-07-15 02:55:04 +0200

    add container card admin bundle view

--- src/components/admin/manage/bundle/tiles/Label.js
+++ src/components/admin/manage/bundle/tiles/Label.js
@@ -5,21 +5,25 @@ import '../../../../utils/css/LabelPdf.css'
 const config = require('../../../../../config.js')
 
 const Label = ( props ) => (
-	<div className="mb-4 text-center">
-		<h3 className="card-title text-left">Etiquette</h3>
-		{props.labelFilename ? <React.Fragment>
-			<button className="mb-1 btn btn-info btn-sm" onClick={props.downloadLabel}>Télécharger</button>
-			<a href={config.cdn_url+'/label/'+props.labelFilename} target="_blank">
-				<Document file={config.cdn_url+'/label/'+props.labelFilename} >
-					<Page pageNumber={1} width={500} className="label" />
-				</Document>
-			</a>
-		</React.Fragment>:(
-			props.loading ?
-				<Loading />:
-				<p>Label non trouvé</p>
-			)
-		}
+	<div className="card mb-4 bg-light">
+		<h4 className="card-header">Etiquette</h4>
+		<div className="card-body p-2 text-center">
+			{props.labelFilename ?
+			<p className="card-text">
+				<button className="mb-1 btn btn-info btn-sm" onClick={props.downloadLabel}>Télécharger</button>
+				<a href={config.cdn_url+'/label/'+props.labelFilename} target="_blank">
+					<Document file={config.cdn_url+'/label/'+props.labelFilename} >
+						<Page pageNumber={1} width={500} className="label" />
+					</Document>
+				</a>
+			</p>:(
+			<p className="card-text">
+				props.loading ?
+					<Loading />:
+					<p>Label non trouvé</p>
+			</p>
+			)}
+		</div>
 	</div>
 );
 
--- src/components/admin/manage/bundle/tiles/Payment.js
+++ src/components/admin/manage/bundle/tiles/Payment.js
@@ -1,17 +1,19 @@
 import React from 'react'
 
 const Payment = ( props ) => (
-	<div className="mb-4 text-center">
-		<h3 className="card-title text-left">Paiement</h3>
-		<div className="form-group">
-			<select className="form-control" onChange={props.changeState} name="state" value={props.state}>
-				<option value="0">Non reglé</option>
-				<option value="1">Paiement en attente de validation</option>
-				<option value="2">Payé</option>
-				<option value="3">Payé et en place</option>
-			</select>
+	<div className="card mb-4 bg-light">
+		<h4 className="card-header">Paiement</h4>
+		<div className="card-body p-2 text-center">
+			<div className="form-group">
+				<select className="form-control" onChange={props.changeState} name="state" value={props.state}>
+					<option value="0">Non reglé</option>
+					<option value="1">Paiement en attente de validation</option>
+					<option value="2">Payé</option>
+					<option value="3">Payé et en place</option>
+				</select>
+			</div>
+			<button className="btn btn-info btn-sm" onClick={props.submitState}>Enregistrer</button>
 		</div>
-		<button className="btn btn-info btn-sm" onClick={props.submitState}>Enregistrer</button>
 	</div>
 )
 
