122ab4c876d915b0e09bca48289477e504d61092
Nicolas Ekszterowicz <nicolas.ekszterowicz@gmail.com>
2018-08-15 12:08:54 +0200

    modif prices septembre 2018, refactorize price page

--- src/components/static/Prices.js
+++ src/components/static/Prices.js
@@ -8,7 +8,9 @@ export default class Prices extends Component {
 	constructor(props) {
 		super(props)
 		ReactGA.pageview(this.props.location.pathname);
+
 	}
+
 
 	render() {
 		return (
@@ -43,13 +45,31 @@ export default class Prices extends Component {
 class IndividualPrices extends Component {
 
 	constructor(props) {
-		super(props)
+		super(props);
 		this.state = {
 			several : false
 		}
+
+		this.indPricesMo = [10, 17, 23, 27, 32];
+		this.indPricesYe = [85, 160, 230, 295, 350];
 	}
 
 	render () {
+		let pricesArr = (this.state.several)? this.indPricesMo : this.indPricesYe;
+
+		let pricesCells = [];
+		for (let price of pricesArr) {
+			let priceDispl;
+			if (this.state.several) {
+				priceDispl = price + '€/mois (' + price * 12 + '€/an)';
+			}
+			else {
+				priceDispl = price + '€/an';
+			}
+
+			pricesCells.push(<td>{priceDispl}</td>);
+		}
+
 		return (
 			<div>
 				<div className="form-check my-4">
@@ -57,7 +77,6 @@ class IndividualPrices extends Component {
 						<input type="checkbox" className="form-check-input" name="several" onClick={handleTick.bind(this)} /> Paiement en plusieurs fois
 					</label>
 				</div>
-				{(this.state.several)?
 				<table className="table">
 					<tbody>
 						<tr>
@@ -73,36 +92,22 @@ class IndividualPrices extends Component {
 							<td>8 pots de 250g</td><td>16 pots de 250g</td><td>24 pots de 250g</td><td>32 pots de 250g</td><td>40 pots de 250g</td>
 						</tr>
 						<tr>
-							<td>8€/mois (96€/an)</td><td>14€/mois (168€/an)</td><td>19€/mois (228€/an)</td><td>23€/mois (276€/an)</td><td>27€/mois (324€/an)</td>
-						</tr>
-					</tbody>
-				</table>:
-				<table className="table">
-					<tbody>
-						<tr>
-							<th>10 000 abeilles</th><th>20 000 abeilles</th><th>30 000 abeilles</th><th>40 000 abeilles</th><th>50 000 abeilles (1 ruche complète)</th>
-						</tr>
-						<tr>
-							<td>Votre nom sur une ruche</td><td>Votre nom sur une ruche</td><td>Votre nom sur une ruche</td><td>Votre nom sur une ruche</td><td>Une ruche à votre nom</td>
-						</tr>
-						<tr>
-							<td>Une page dédiée à la ruche avec photos & actualités</td><td>Une page dédiée à la ruche avec photos & actualités</td><td>Une page dédiée à la ruche avec photos & actualités</td><td>Une page dédiée à la ruche avec photos & actualités</td><td>Une page dédiée à votre ruche avec photos & actualités</td>
-						</tr>
-						<tr>
-							<td>8 pots de 250g</td><td>16 pots de 250g</td><td>24 pots de 250g</td><td>32 pots de 250g</td><td>40 pots de 250g</td>
-						</tr>
-						<tr>
-							<td>70€/an</td><td>135€/an</td><td>195€/an</td><td>250€/an</td><td>300€/an</td>
+							{pricesCells}
 						</tr>
 					</tbody>
 				</table>
-				}
 			</div>
 		);
 	}
 }
 
 class CompanyPrices extends Component {
+
+	constructor(props) {
+		super(props);
+
+		this.entrPricesYe = [635, 560];
+	}
 
 	render () {
 		return (
@@ -124,8 +129,8 @@ class CompanyPrices extends Component {
 						<td>80 pots de miel de 125g par ruche</td>
 					</tr>
 					<tr>
-						<td>545€ HT /ruche</td>
-						<td>495€ HT /ruche</td>
+						<td>{this.entrPricesYe[0]}€ HT /ruche</td>
+						<td>{this.entrPricesYe[1]}€ HT /ruche</td>
 					</tr>
 				</tbody>
 			</table>
