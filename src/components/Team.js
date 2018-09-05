6450a3b8c5b8cd6b7eeeeaa18c8038f83562338a
Nicolas Ekszterowicz <nicolas.ekszterowicz@gmail.com>
2018-09-05 20:04:17 +0200

    update all team

--- src/components/Team.js
+++ src/components/Team.js
@@ -2,8 +2,10 @@ import React, { Component } from 'react'
 import Gaetan from '../assets/img/gaetan.jpg'
 import Nico from '../assets/img/nico.jpg'
 import Clem from '../assets/img/clement.jpg'
+import Mick from '../assets/img/mickael.jpg'
 import Benoit from '../assets/img/benoit.jpg'
 import Lea from '../assets/img/lea.jpg'
+import Marion from '../assets/img/marion.jpg'
 import Profile from '../assets/img/profile.png'
 import { Link } from 'react-router-dom'
 import FontAwesome from 'react-fontawesome'
@@ -35,7 +37,8 @@ export default class Team extends Component {
 							<a href="https://medium.com/@geksz" target="_blank" rel="noopener noreferrer"><FontAwesome name='medium' size="2x" /></a>
 						</p>
 						<p className="text-justify">
-							Gaëtan est en dernière année d’étude à l’Ecole des Mines. Une école d’ingénieur généraliste dans laquelle il est rentré à la suite d’une classe préparatoire en Physique-Chimie. Apiculteur depuis l’âge de 14 ans, il a co-fondé Confidences d’Abeilles en 2015. En tant que président, il a développé la marque et son image en s’occupant entre autres de la communication, du branding et du design. Il est par ailleurs responsable de la production, de la qualité, de la commercialisation et des partenariats de plus en plus nombreux.<br />Passionné d’apiculture, il rédige les articles de vulgarisation du blog et continue de superviser l’association d’apiculture qu’il a fondé sur son campus étudiant.<br />Le projet de parrainage de ruches est pour lui un premier pas. Un premier pas vers la création d’une communauté sociale apicole de personnes averties ; amateurs, professionnels, parrains œuvrant ensemble pour changer la donne. Des synergies avec d’autres acteurs sont déjà en place.
+							Gaëtan poursuit ses études en mastère spécialisé entrepreneurs à Grenoble Ecole de Management. Il complète ainsi son parcours d’ingénieur généraliste des Mines, une école qu’il avait intégrée à la suite d’une classe préparatoire en Physique-Chimie. Apiculteur depuis l’âge de 14ans, il a co-fondé Confidences d’Abeilles. En tant qu’instigateur, il a développé la marque, ses services, son image en s’occupant entre autres de la communication, du marketing et du design. Il est par ailleurs responsable de la production, de la qualité, de la commercialisation et des partenariats conclus avec les sous-traitants de la société.<br/>
+							Passionné d’apiculture, c’est lui qui rédige la majorité des articles de vulgarisation du blog et il reste président d’honneur de l’association d’apiculture qu’il a fondée sur son campus étudiant.
 						</p>
 					</div>
 					<div className="col-lg-5 col-md-10 col-sm-12 text-center mt-4">
@@ -69,29 +72,31 @@ export default class Team extends Component {
 						</p>
 					</div>
 					<div className="col-lg-5 col-md-10 col-sm-12 text-center mt-4">
-						<img src={Profile} alt="Someone" className="w-50 rounded-circle"/>
-						<h3 className="text-center mt-4">KEVIN</h3>
-						<h4 className="text-center mb-4"><small>WEB DEVELOPER</small></h4>
+						<img src={Mick} alt="Mickael" className="w-50 rounded-circle"/>
+						<h3 className="text-center mt-4">MICKAEL PHILIPOT</h3>
+						<h4 className="text-center mb-4"><small>BUSINESS DEVELOPPER</small></h4>
 						<p className="text-center">
+							<a href="https://www.facebook.com/mickael.philipot" target="_blank" rel="noopener noreferrer"><FontAwesome name='facebook-official' size="2x" /></a>&nbsp;&nbsp;&nbsp;
+							<a href="https://twitter.com/mickaelphilipot" target="_blank" rel="noopener noreferrer"><FontAwesome name='twitter' size="2x" /></a>&nbsp;&nbsp;&nbsp;
+							<a href="mailto:mickael@confidencesdabeilles.fr"><FontAwesome name='envelope-o' size="2x" /></a>&nbsp;&nbsp;&nbsp;
+							<a href="https://www.linkedin.com/in/mickael-philipot-083475144" target="_blank" rel="noopener noreferrer"><FontAwesome name='linkedin' size="2x" /></a>
 						</p>
 						<p className="text-justify">
-							Kevin est lui aussi étudiant à l'école 42. Plus que notre stagiaire web development il seconde Clément dans réalisation de la plateforme et se charge d'implémenter les nombreuses fonctionnalités à venir.
-							<br /><br />
-							Kevin est un excellent grimpeur que vous pourrez croiser sur un bloc parisien.
+							Après des années dans le sport de haut niveau, Mickael a décidé de ranger ces spatules pour s'engager sur un nouveau projet. Détenteur d'une licence Management de la Relation Commerciale à l'IUT d'Annecy-le-vieux, il compte aujourd'hui mettre en pratique ses connaissance et aussi beaucoup apprendre du milieu de l'apiculture. Cet amateur de miel depuis toujours est aujourd'hui en charge du développement Commercial et Marketing chez Confidences d'Abeilles.
 						</p>
 					</div>
 				</div>
 				<div className="row justify-content-around">
 					<div className="col-lg-5 col-md-10 col-sm-12 text-center mt-4">
-						<img src={Lea} alt="Gaetan" className="w-50 rounded-circle"/>
+						<img src={Lea} alt="Lea" className="w-50 rounded-circle"/>
 						<h3 className="text-center mt-4">LEA</h3>
-						<h4 className="text-center mb-4"><small>MARKETING & COMMUNICATION ASSISTANT</small></h4>
+						<h4 className="text-center mb-4"><small>MARKETING & COMMUNICATION</small></h4>
 						<p className="text-center">
 							<a href="mailto:lea@confidencesdabeilles.fr"><FontAwesome name='envelope-o' size="2x" /></a>&nbsp;&nbsp;&nbsp;
 							<a href="https://www.linkedin.com/in/léa-sengèle-147b63149" target="_blank" rel="noopener noreferrer"><FontAwesome name='linkedin' size="2x" /></a>
 						</p>
 						<p className="text-justify">
-							Etudiante à l'IAE de Annecy-le-Vieux en dernière année de licence Marketing et communication, Léa, notre jeune stagiaire et amoureuse des produits de la ruche, est aujourd'hui en charge du développement du service de parrainage ainsi que de la communication chez Confidences d'Abeilles.
+							Etudiante à l'IAE de Toulouse en master, Léa, amoureuse des produits de la ruche, est aujourd'hui en charge du développement du service de parrainage ainsi que de la communication chez Confidences d'Abeilles.
 						</p>
 					</div>
 					<div className="col-lg-5 col-md-10 col-sm-12 text-center mt-4">
@@ -110,6 +115,22 @@ export default class Team extends Component {
 					</div>
 				</div>
 				<div className="row justify-content-around">
+					<div className="col-lg-5 col-md-10 col-sm-12 text-center mt-4">
+						<img src={Marion} alt="Marion" className="w-50 rounded-circle"/>
+						<h3 className="text-center mt-4">MARION LETUR</h3>
+						<h4 className="text-center mb-4"><small>GRAPHISTE & WEBDESIGNER</small></h4>https://www.behance.net/leturm
+						<p className="text-center">
+							<a href="https://www.instagram.com/larecruedesign/" target="_blank" rel="noopener noreferrer"><FontAwesome name='instagram' size="2x" /></a>&nbsp;&nbsp;&nbsp;
+							<a href="https://fr-fr.facebook.com/larecruedesign/" target="_blank" rel="noopener noreferrer"><FontAwesome name='facebook-official' size="2x" /></a>&nbsp;&nbsp;&nbsp;
+							<a href="http://marionletur.com/" target="_blank" rel="noopener noreferrer"><FontAwesome name='globe' size="2x" /></a>&nbsp;&nbsp;&nbsp;
+							<a href="https://www.linkedin.com/in/marion-letur-642427107/" target="_blank" rel="noopener noreferrer"><FontAwesome name='linkedin' size="2x" /></a>&nbsp;&nbsp;&nbsp;
+							<a href="https://www.behance.net/leturm" target="_blank" rel="noopener noreferrer"><FontAwesome name='behance' size="2x" /></a>
+						</p>
+						<p className="text-justify">
+							Illustratrice freelance depuis Janvier 2016. Vive, créative et dévouée dans son travail, elle a intégré la team Confidences d'Abeilles en tant que Graphiste-Webdesigner.<br/>
+							Vous pourrez trouver Marion sur un sommet de Bellevaux puisqu'elle aime crapahuter, sur une scène dans un bar puisqu'elle est chanteuse, ou juste dans un bar parce que c'est quand même sympa de profiter d'une bonne bière avec les copains.
+						</p>
+					</div>
 					<div className="col-lg-5 col-md-10 col-sm-12 text-center mt-4">
 						<img src={Profile} alt="Someone" className="w-50 rounded-circle"/>
 						<h3 className="text-center mt-4">VOUS</h3>
