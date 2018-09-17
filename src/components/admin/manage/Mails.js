20fd1cbf1ed77f8260007a30ef09056dc17f7af1
Nicolas Ekszterowicz <nicolas.ekszterowicz@gmail.com>
2018-09-17 13:36:39 +0200

    modifs, improve admin send mail panel

--- src/components/admin/manage/Mails.js
+++ src/components/admin/manage/Mails.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import request from '../../../services/Net';
 import NotificationSystem from 'react-notification-system';
-import { handleChange } from '../../../services/FormService';
 
 
 export default class AdminManageMails extends Component {
@@ -9,20 +8,48 @@ export default class AdminManageMails extends Component {
 	constructor(props) {
 		super(props);
 		this.state = {
-			emailNumber: '',
+			emailNumber: undefined,
+			availableEmailNbLst: [],
 			userId: undefined,
-			userNameList: []
-		}
+			userNameList: [],
+			saveDataField: false,
+		};
 	}
 
 	componentDidMount() {
+		let saveDataField = null, emailNumber = null, userId = null;
+		saveDataField = localStorage.getItem("testMail-saveDataField") === 'true';
+		console.log('localStorage.getItem("testMail-userId") : ' + localStorage.getItem("testMail-userId"));
+		if (saveDataField) {
+			this.setState({
+				saveDataField: saveDataField
+			})
+			emailNumber = localStorage.getItem("testMail-emailNumber");
+			userId = localStorage.getItem("testMail-userId");
+		}
+
+		request({
+			url : '/mails/available',
+			method : 'GET'
+		}, this.refs.notif).then((res) => {
+			if (!emailNumber) {
+				emailNumber = res[0]
+			}
+			this.setState({
+				availableEmailNbLst : res,
+				emailNumber : emailNumber
+			});
+		});
 		request({
 			url : '/user',
 			method : 'GET'
 		}, this.refs.notif).then((res) => {
+			if (!userId) {
+				userId = res[0].id
+			}
 			this.setState({
 				userNameList : res,
-				userId : res[0].id
+				userId : userId
 			});
 		});
 	}
@@ -34,8 +61,42 @@ export default class AdminManageMails extends Component {
 		});
 	}
 
+	changedDataRegister = (event) => {
+		const target = event.target;
+	    const name = target.name;
+	    const value = target.type === 'checkbox' ? target.checked : target.value;
+
+	    this.setState({
+	        [name]: value
+	    });
+
+		if (name === 'saveDataField') {
+			// register value
+			localStorage.setItem("testMail-" + name, value);
+
+			if (value === true) {
+				localStorage.setItem("testMail-emailNumber", this.state.emailNumber);
+				localStorage.setItem("testMail-userId", this.state.userId);
+			} else {
+				localStorage.removeItem("testMail-saveDataField");
+				localStorage.removeItem("testMail-emailNumber");
+				localStorage.removeItem("testMail-userId");
+				// reset fields
+				this.setState((state) => {
+					return {
+						emailNumber: state.availableEmailNbLst[0],
+						userId: state.userNameList[0].id
+					};
+				});
+			}
+		} else if (this.state.saveDataField) {
+			// request to save data
+			// register value
+			localStorage.setItem("testMail-" + name, value);
+		}
+	}
+
 	sendMailUser = (ev) => {
-		ev.preventDefault()
 		request({
 			url : '/mail',
 			method : 'POST',
@@ -44,6 +105,7 @@ export default class AdminManageMails extends Component {
 				userId: this.state.userId
 			}
 		}, this.refs.notif);
+		ev.preventDefault();
 	}
 
 	render () {
@@ -52,17 +114,24 @@ export default class AdminManageMails extends Component {
 				<NotificationSystem ref="notif" />
 				{/*gfdgdfg<button className="btn btn-danger" onClick={this.launch} >Launch email</button>*/}
 				<h2>Test mails</h2>
-				<form onSubmit={this.sendMailUser}>
+				<form>
 					<label>Identifiant du mail à envoyer :</label>
-					<input type="text" name="emailNumber" onChange={handleChange.bind(this)} value={this.state.emailNumber} className="form-control form-control-sm" />
-					<label>User :</label>
-					<select name="userId" onChange={handleChange.bind(this)} value={this.state.userId} className="form-control">
-						{this.state.userNameList.map((user) => <option value={user.id} key={user.id}>
-								{user.name + ' ' + user.firstname}
+					<select name="emailNumber" onChange={this.changedDataRegister} value={this.state.emailNumber} className="form-control form-control-sm">
+						{this.state.availableEmailNbLst.map((emailNb) => <option value={emailNb} key={emailNb}>
+								{emailNb}
 							</option>)
 						}
 					</select>
-					<button className="btn btn-danger">Send Mail</button>
+					<label>User :</label>
+					<select name="userId" onChange={this.changedDataRegister} value={this.state.userId} className="form-control form-control-sm">
+						{this.state.userNameList.map((user) => {
+							return <option value={user.id} key={user.id}>
+									{user.name + ' ' + user.firstname}
+								</option>;
+						})}
+					</select>
+					<button type="submit" className="btn btn-danger mt-2" onClick={this.sendMailUser}>Send Mail</button>
+					<label className="ml-3">Sauvegarder les champs <input type="checkbox" name="saveDataField" checked={this.state.saveDataField} onChange={this.changedDataRegister} /></label>
 				</form>
 			</div>
 		)
