8f3c1ec132eb0f199c9351684624395562cfaf15
Clément CHAMPOUILLON <clement.champouillon@link-value.fr>
2018-12-09 09:43:48 +0100

    Meta update

--- config.js
+++ config.js
@@ -1,4 +1,6 @@
 
 module.exports = {
   mongoUrl: 'mongodb://abeilles:d9ucfJ2AgVecdxFm@localhost:27017/confidences',
+  baseDomain: 'https://parrainagederuches.fr',
+  contentDomain: 'https://cdn.parrainagederuches.fr',
 };
