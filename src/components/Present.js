8396ebaf8a56f65cbcc5b0a9664efc8dd9daba21
Clément CHAMPOUILLON <clement.champouillon@link-value.fr>
2018-12-19 23:13:07 +0100

    refactor(signup)

--- src/components/Present.js
+++ src/components/Present.js
@@ -1,163 +1,141 @@
 import React, { Component } from 'react'
-import ReactGA from 'react-ga';
 import presentPict from '../assets/img/present.jpg'
 import { Link } from 'react-router-dom'
 import Meta from './utils/Meta'
 
-export default class Present extends Component {
-
-    constructor(props) {
-        super (props)
-        ReactGA.pageview(this.props.location.pathname);
-    }
-
-    render () {
-        return (
-          <div className="container">
-            <Meta title="Offrir un parrainage"/>
-            <div className="row mb-4">
-                <div className="col">
-                    <img className="img-fluid" src={presentPict} alt="Bannière cadeau" />
-                </div>
-            </div>
-            <div className="row">
-                <div className="col">
-                    <h2 className="text-center my-4">Parrainer des abeilles : un cadeau aussi utile qu’original !</h2>
-                </div>
-            </div>
-            <div className="row justify-content-center">
-                <div className="col-lg-9 col-md-10 col-sm-12">
-                    <p className="mb-5">
-                        <strong>Vous cherchiez un cadeau original qu’on puisse à la fois toucher du regard, expérimenter, déguster et qui soit
-                        concrètement bénéfique pour notre
-                        environnement ?</strong> Alors vous êtes au bon
-                        endroit ! En effet, le parrainage d’Abeilles ou
-                        de ruches a la particularité de réunir tous ces
-                        atouts !
-                    </p>
-                </div>
-            </div>
-    				<div className="row justify-content-center">
-    					<div className="col-lg-9 col-md-10 col-sm-12">
-    						<ul className="nav nav-tabs justify-content-center" role="tablist">
-    							<li className="nav-item">
-    								<a className="nav-link active lead" data-toggle="tab" href="#how-offer">Comment offrir un parrainage ?</a>
-    							</li>
-    							<li className="nav-item">
-    								<a className="nav-link lead" data-toggle="tab" href="#presentation">Offrir un parrainage, c’est quoi ?</a>
-    							</li>
-    						</ul>
-    						<div className="tab-content" style={{ overflowX : 'auto' }}>
-    							<div id="how-offer" className="tab-pane active" role="tabpanel">
-    								<HowOfferPresent />
-    							</div>
-    							<div id="presentation" className="tab-pane" role="tabpanel">
-    								<PresentationPresent />
-    							</div>
-    						</div>
-    					</div>
-    				</div>
-    			</div>
-        )
-    }
-}
-
-class HowOfferPresent extends Component {
-
-	render () {
-    return (
-      <div className="container mt-4">
-        <div className="row">
-            <div className="col">
-              <p className="">
-                  Dans le but de garantir une bonne saisie des
-                  informations <strong>nous vous invitons à lire toutes
-                  ces étapes avant de commencer.</strong>
-              </p>
-              <ol className="lead">
-                  <li className="my-4">
-                      Inscrivez vous avec vos propres
-                      informations (elles sont nécessaires à la
-                      gestion de votre compte / à la
-                      facturation)
-                  </li>
-                  <li className="my-4">
-                      Choisissez un nombre d’abeilles à offrir
-                  </li>
-                  <li className="my-4">
-                      Désignez votre bénéficiaire en cochant
-                      « Ce parrainage est un cadeau » et en
-                      renseignant ses coordonnées.
-
-                  </li>
-                  <li className="my-4">
-                  ATTENTION à la date à partir de
-                  laquelle doit être notifié le bénéficiaire.<br /><strong>Un délai de 3 jours est recommandé.</strong>
-                  </li>
-                  <li className="my-4">
-                      Terminez en réglant votre cadeau (la
-                      confirmation de règlement est
-                      nécessaire pour la génération du
-                      parrainage)
-                  </li>
-                  <li className="my-4">
-                      <span className="align-middle">C’est terminé </span>
-                      <img src={require('../assets/img/smiley/happy.svg')} alt="smiley happy"
-        								style={{ height: '1em' }} />
-                  </li>
-              </ol>
-            </div>
+export default () => (
+  <div className="container">
+    <Meta title="Offrir un parrainage"/>
+    <div className="row mb-4">
+        <div className="col">
+            <img className="img-fluid" src={presentPict} alt="Bannière cadeau" />
+        </div>
+    </div>
+    <div className="row">
+        <div className="col">
+            <h2 className="text-center my-4">Parrainer des abeilles : un cadeau aussi utile qu’original !</h2>
+        </div>
+    </div>
+    <div className="row justify-content-center">
+        <div className="col-lg-9 col-md-10 col-sm-12">
+            <p className="mb-5">
+                <strong>Vous cherchiez un cadeau original qu’on puisse à la fois toucher du regard, expérimenter, déguster et qui soit
+                concrètement bénéfique pour notre
+                environnement ?</strong> Alors vous êtes au bon
+                endroit ! En effet, le parrainage d’Abeilles ou
+                de ruches a la particularité de réunir tous ces
+                atouts !
+            </p>
+        </div>
+    </div>
+    <div className="row justify-content-center">
+      <div className="col-lg-9 col-md-10 col-sm-12">
+        <ul className="nav nav-tabs justify-content-center" role="tablist">
+          <li className="nav-item">
+            <a className="nav-link active lead" data-toggle="tab" href="#how-offer">Comment offrir un parrainage ?</a>
+          </li>
+          <li className="nav-item">
+            <a className="nav-link lead" data-toggle="tab" href="#presentation">Offrir un parrainage, c’est quoi ?</a>
+          </li>
+        </ul>
+        <div className="tab-content" style={{ overflowX : 'auto' }}>
+          <div id="how-offer" className="tab-pane active" role="tabpanel">
+            <HowOfferPresent />
           </div>
-          <div className="row">
-            <div className="col">
-              <p className="text-center">
-                  <Link to="/signup/individual" className="btn btn-secondary">J'offre un parrainage<br/>d’abeilles</Link>
-              </p>
-            </div>
+          <div id="presentation" className="tab-pane" role="tabpanel">
+            <PresentationPresent />
+          </div>
         </div>
       </div>
-		);
-	}
-}
+    </div>
+  </div>
+);
+const HowOfferPresent = () => (
+  <div className="container mt-4">
+    <div className="row">
+        <div className="col">
+          <p className="">
+              Dans le but de garantir une bonne saisie des
+              informations <strong>nous vous invitons à lire toutes
+              ces étapes avant de commencer.</strong>
+          </p>
+          <ol className="lead">
+              <li className="my-4">
+                  Inscrivez vous avec vos propres
+                  informations (elles sont nécessaires à la
+                  gestion de votre compte / à la
+                  facturation)
+              </li>
+              <li className="my-4">
+                  Choisissez un nombre d’abeilles à offrir
+              </li>
+              <li className="my-4">
+                  Désignez votre bénéficiaire en cochant
+                  « Ce parrainage est un cadeau » et en
+                  renseignant ses coordonnées.
 
-class PresentationPresent extends Component {
-
-	render () {
-		return (
-      <div className="container mt-4">
-        <div className="row">
-            <div className="col">
-              <p>
-                Votre cadeau permet à l’heureux bénéficiaire de devenir parrain de 10 000 abeilles (ou
-                plus). 10 000, tant que ça ?! Eh oui, il faut savoir qu’une ruche compte en moyenne 50 000
-                individus. Des informations comme celle-ci mais aussi des photos de leurs abeilles, les
-                heureux parrains en recevront tout au long de l’année que dure leur parrainage. Il y a
-                beaucoup à apprendre sur ce monde fascinant et plus captivant encore. Ils auront aussi la
-                possibilité de prendre part aux visites des ruches au cours de l’année ! L’occasion pour eux
-                d’enfiler la fameuse combinaison de l’apiculteur et de plonger littéralement au cœur de la
-                colonie !
-              </p>
-              <p>
-                Enfin, après avoir bien travaillé, les abeilles partageront une partie de leur trésor pour le
-                plus grand plaisir des petits et des grands. Un moment gastronomique inoubliable.
-              </p>
-              <p>
-                <strong>Par votre cadeau, les parrains prennent plaisir à suivre l’évolution de leur ruche via une
-                page dédiée et participent également à la sauvegarde d’un métier et d’une espèce menacée.</strong><br />
-                Les abeilles, par leur activité de pollinisation, protègent la biodiversité et préservent donc
-                notre environnement. Celui-ci étant plus fécond il nécessite moins de traitements
-                nuisibles aux abeilles. C’est un cercle vertueux qu’il faut réussir à amorcer !
-              </p>
-            </div>
-        </div>
-        <div className="row">
-            <div className="col">
-              <p className="text-center" >
-                  <Link to="/individual/more" className="btn btn-secondary">En savoir plus</Link>
-              </p>
-            </div>
+              </li>
+              <li className="my-4">
+              ATTENTION à la date à partir de
+              laquelle doit être notifié le bénéficiaire.<br /><strong>Un délai de 3 jours est recommandé.</strong>
+              </li>
+              <li className="my-4">
+                  Terminez en réglant votre cadeau (la
+                  confirmation de règlement est
+                  nécessaire pour la génération du
+                  parrainage)
+              </li>
+              <li className="my-4">
+                  <span className="align-middle">C’est terminé </span>
+                  <img src={require('../assets/img/smiley/happy.svg')} alt="smiley happy"
+                    style={{ height: '1em' }} />
+              </li>
+          </ol>
         </div>
       </div>
-		);
-	}
-}
+      <div className="row">
+        <div className="col">
+          <p className="text-center">
+              <Link to="/signup/individual" className="btn btn-secondary">J'offre un parrainage<br/>d’abeilles</Link>
+          </p>
+        </div>
+    </div>
+  </div>
+);
+
+const PresentationPresent = () => (
+  <div className="container mt-4">
+    <div className="row">
+        <div className="col">
+          <p>
+            Votre cadeau permet à l’heureux bénéficiaire de devenir parrain de 10 000 abeilles (ou
+            plus). 10 000, tant que ça ?! Eh oui, il faut savoir qu’une ruche compte en moyenne 50 000
+            individus. Des informations comme celle-ci mais aussi des photos de leurs abeilles, les
+            heureux parrains en recevront tout au long de l’année que dure leur parrainage. Il y a
+            beaucoup à apprendre sur ce monde fascinant et plus captivant encore. Ils auront aussi la
+            possibilité de prendre part aux visites des ruches au cours de l’année ! L’occasion pour eux
+            d’enfiler la fameuse combinaison de l’apiculteur et de plonger littéralement au cœur de la
+            colonie !
+          </p>
+          <p>
+            Enfin, après avoir bien travaillé, les abeilles partageront une partie de leur trésor pour le
+            plus grand plaisir des petits et des grands. Un moment gastronomique inoubliable.
+          </p>
+          <p>
+            <strong>Par votre cadeau, les parrains prennent plaisir à suivre l’évolution de leur ruche via une
+            page dédiée et participent également à la sauvegarde d’un métier et d’une espèce menacée.</strong><br />
+            Les abeilles, par leur activité de pollinisation, protègent la biodiversité et préservent donc
+            notre environnement. Celui-ci étant plus fécond il nécessite moins de traitements
+            nuisibles aux abeilles. C’est un cercle vertueux qu’il faut réussir à amorcer !
+          </p>
+        </div>
+    </div>
+    <div className="row">
+        <div className="col">
+          <p className="text-center" >
+              <Link to="/individual/more" className="btn btn-secondary">En savoir plus</Link>
+          </p>
+        </div>
+    </div>
+  </div>
+);
---
+++ src/components/Signup.jsx
@@ -0,0 +1,194 @@
+import React, { Component } from 'react'
+import { Link, Redirect } from 'react-router-dom'
+import { handleChange } from '../services/FormService'
+import { login, isLoggedIn } from '../services/AuthService'
+import request from '../services/Net.js'
+import NotificationSystem from 'react-notification-system';
+import Meta from './utils/Meta';
+
+export default class Signup extends Component {
+  constructor(props) {
+    super(props);
+    const { match : { params } } = props;
+    this.state = {
+      sexe_m: '',
+      firstname: '',
+      name: '',
+      email: '',
+      phone: '',
+      school: '',
+      password: '',
+      confirmation: '',
+      user_type: this.getType(params.type),
+      message: '',
+      redirect: false,
+    };
+  }
+
+	componentDidMount() {
+		if (isLoggedIn(true)) {
+			this.setState({ redirect : 'account' })
+		}
+	}
+
+	getType(type) {
+		switch (type) {
+			case 'individual':
+				return 1;
+			case 'company':
+				return 2;
+			case 'contributor':
+				return 3;
+			default:
+				return 0;
+		}
+	}
+
+  register = (e) => {
+    e.preventDefault();
+    const {
+      password,
+      confirmation,
+      sexe_m,
+      firstname,
+      name,
+      email,
+      phone,
+      school,
+      user_type,
+    } = this.state;
+    if (password !== confirmation) {
+      this.refs.notificationSystem.addNotification({
+        message: 'Le mot de passe et sa confirmation ne sont pas identiques',
+        level: 'error',
+      });
+    } else {
+      request({
+        method: 'post',
+        url: '/user',
+        data: {
+          sexe_m,
+          firstname,
+          name,
+          email,
+          phone,
+          school,
+          password,
+          user_type,
+        },
+      }, this.refs.notificationSystem)
+        .then(() => {
+          request({
+            url: '/authenticate',
+            method: 'post',
+            data: {
+              email,
+              password,
+            },
+          }, this.refs.notificationSystem).then(({ id, token, user_type: uT }) => {
+            login(id, token, uT);
+            if (uT === 2) {
+              this.setState({
+                redirect: 'company/identity',
+              });
+            } else if (uT === 1) {
+              this.setState({
+                redirect: 'individual/address',
+              });
+            } else if (uT === 3) {
+              this.setState({
+                redirect: 'contributor/address',
+              });
+            }
+          })
+        })
+    }
+  }
+
+  render() {
+    const {
+      user_type: userType,
+      redirect,
+      message,
+      sexe_m,
+    } = this.state;
+    const linkInfoPresent = () => (userType === 1) && (
+      <Link to="/present">
+        <button type="button" className="btn btn-link btn-sm">
+          Comment offrir un parrainage en 6 étapes ?
+        </button>
+      </Link>
+    );
+
+    return (
+      <div className="container py-4">
+        <Meta title="Inscription"/>
+        {redirect && <Redirect to={`/${redirect}`} />}
+        <NotificationSystem ref="notificationSystem" />
+        <div className="row justify-content-center">
+          <div className="col">
+            <div className="progress">
+              <div className="progress-bar" role="progressbar" style={{ width: '25%' }} />
+            </div>
+          </div>
+        </div>
+        <div className="row justify-content-center">
+          <div className="col-lg-6 col-md-10 col-sm-12">
+            <form className="text-center">
+              <h2 className="text-center my-4">Créer votre compte</h2>
+              {message && <p className="alert alert-danger">{message}</p>}
+              <div className="form-group d-flex">
+                <label className="radio-inline form-check-label">
+                  <input
+                    type="radio"
+                    className="form-check-input"
+                    name="sexe_m"
+                    value="1"
+                    onChange={handleChange.bind(this)}
+                    checked={sexe_m === '1'}
+                    autoComplete="sex"
+                  />
+                  &nbsp;M *
+                </label>
+                <label className="radio-inline form-check-label ml-4">
+                  <input type="radio" className="form-check-input" name="sexe_m" value="0" onChange={handleChange.bind(this)} checked={this.state.sexe_m === '0'} autoComplete="sex" />
+                  &nbsp;Mme *
+                </label>
+              </div>
+              <div className="form-group">
+                <input type="text" name="firstname" className="form-control" placeholder="Prénom *" onChange={handleChange.bind(this)} autoComplete="given-name" />
+              </div>
+              <div className="form-group">
+                <input type="text" name="name" className="form-control" placeholder="Nom *" onChange={handleChange.bind(this)} autoComplete="family-name" />
+              </div>
+              <div className="form-group">
+                <input type="email" name="email" className="form-control" placeholder="Adresse email *" onChange={handleChange.bind(this)} autoComplete="email" />
+              </div>
+              <div className="form-group">
+                <input
+                  type={(userType === 3)?'text':'tel'}
+                  name={(userType === 3)?'school':'phone'}
+                  className="form-control"
+                  placeholder={(userType === 3)?'Ecole ou établissement':'Numéro de telephone *'}
+                  onChange={handleChange.bind(this)}
+                  autoComplete={(userType === 3)?'organization':'tel'}
+                />
+              </div>
+              <div className="form-group">
+                <input type="password" name="password" className="form-control" placeholder="Mot de passe *" onChange={handleChange.bind(this)} autoComplete="new-password" />
+              </div>
+              <div className="form-group">
+                <input type="password" name="confirmation" className="form-control" placeholder="Confirmation du mot de passe *" onChange={handleChange.bind(this)} />
+              </div>
+              <input type="submit" value="Valider" className="btn btn-primary" onClick={this.register} />
+            </form>
+            <p className="mt-3 text-center">
+              Vous avez déjà un compte ? <Link to="/login">Connectez vous</Link><br />
+              {linkInfoPresent()}
+            </p>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
