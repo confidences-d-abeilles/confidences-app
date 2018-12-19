26df88df15d6b65bcd261557ff1faf82cd490866
Clément CHAMPOUILLON <clement.champouillon@link-value.fr>
2018-12-19 21:39:00 +0100

    refactor(global)

--- src/services/Net.js
+++ src/services/Net.js
@@ -1,43 +1,40 @@
 import axios from 'axios';
 
 export const client = axios.create({
-	baseURL: process.env.REACT_APP_API_DOMAIN,
+  baseURL: process.env.REACT_APP_API_DOMAIN,
 });
 
-const request = function(options, notificationSystem) {
-	const onSuccess = function(response) {
-		if (response.data.message && notificationSystem) {
-			notificationSystem.addNotification({
-				message: response.data.message,
-				level: 'success'
-			});
-		}
-		return response.data.payload;
-	}
+export default (options, notificationSystem) => {
+  const onSuccess = (response) => {
+    if (response.data.message && notificationSystem) {
+      notificationSystem.addNotification({
+        message: response.data.message,
+        level: 'success',
+      });
+    }
+    return response.data.payload;
+  };
 
-	const onError = function(error) {
-		if (error.response && notificationSystem) {
-			if (error.response.status === '400') {
-				notificationSystem.addNotification({
-					message: error.response.data,
-					level: 'warning'
-				});
-			} else {
-				notificationSystem.addNotification({
-					message: error.response.data,
-					level: 'error'
-				});
-			}
-		} else {
-		// Something else happened while setting up the request
-		// triggered the error
-		console.error('Error Message:', error.message);
-		}
+  const onError = (error) => {
+    if (error.response && notificationSystem) {
+      if (error.response.status === '400') {
+        notificationSystem.addNotification({
+          message: error.response.data,
+          level: 'warning',
+        });
+      } else {
+        notificationSystem.addNotification({
+          message: error.response.data,
+          level: 'error',
+        });
+      }
+    } else {
+      // eslint-disable-next-line no-console
+      console.error('Error Message:', error.message);
+    }
 
-		return Promise.reject(error.response || error.message);
-	}
+    return Promise.reject(error.response || error.message);
+  };
 
-	return client(options).then(onSuccess).catch(onError);
-}
-
-export default request;
+  return client(options).then(onSuccess).catch(onError);
+};
