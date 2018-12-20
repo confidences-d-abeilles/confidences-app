0a6923f9aefa4c33148bc2ddad729df191403968
Clément CHAMPOUILLON <clement.champouillon@link-value.fr>
2018-12-20 22:29:10 +0100

    refacto(header)

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,13 @@
-import React, { Component } from 'react';
-import { NavLink, Link, Redirect } from 'react-router-dom';
+import React, { Component, Fragment } from 'react';
+import { Link, Redirect } from 'react-router-dom';
 import NotificationSystem from 'react-notification-system';
 import FontAwesome from 'react-fontawesome';
 import logoSquare from '../assets/img/logo-square.png';
 import { isLoggedIn } from '../services/AuthService';
 import { handleChange } from '../services/FormService';
 import request from '../services/Net';
+import MyLink from './utils/Link';
+import navLinks from '../config/navLinks';
 
 export default class Header extends Component {
   state = {
@@ -38,7 +40,7 @@ export default class Header extends Component {
   newsletterPopup = () => (
     <div className="modal fade" id="newsmodal" data-backdrop={false} ref="newsmodal">
       <div className="modal-dialog" role="document">
-        <form className="modal-content" onSubmit={this.subNewsletter} >
+        <form className="modal-content" onSubmit={this.subNewsletter}>
           <div className="modal-header">
             <h5 className="modal-title">S'abonner à la newsletter</h5>
             <button type="button" className="close" data-dismiss="modal" aria-label="Close">
@@ -68,7 +70,7 @@ export default class Header extends Component {
         <NotificationSystem ref="notif" />
         <nav className="navbar navbar-toggleable-md navbar-light" style={{ boxShadow: '0 2px 2px silver' }}>
           <button className="navbar-toggler navbar-toggler-right align-self-center" type="button" data-toggle="collapse" data-target="#navbarNav">
-            <span className="navbar-toggler-icon"></span>
+            <span className="navbar-toggler-icon" />
           </button>
           {redirect && <Redirect to="/" />}
           <Link className="navbar-brand" to="/">
@@ -76,192 +78,100 @@ export default class Header extends Component {
             <h2 className="badge badge-danger" style={{ position: 'absolute', top: '75px', left: '75px', fontSize: '1.5em', zIndex: 1000 }}>{(process.env.NODE_ENV === "development")?'Bêta':null}</h2>
           </Link>
           <div className="hidden-lg-up collapse" style={{justifyContent: 'space-between'}} id="navbarNav" onClick={() => { document.getElementById("navbarNav").classList.remove("show") }}>
-            {isLoggedIn() ? (
-              <ul className="navbar-nav">
-                <div className="dropdown-divider" />
-              </ul>
-            ) : (
-              <ul className="navbar-nav">
+            <ul className="navbar-nav">
+              {isLoggedIn() ? navLinks.mobile.loggedIn.map(props => (
                 <li className="nav-item">
-                  <Link className="nav-link" to="/company/presentation">Entreprise</Link>
-                </li>
-                <li className="nav-item">
-                  <Link className="nav-link" to="/individual/presentation">Particulier</Link>
-                </li>
-                <li className="nav-item">
-                  <Link className="nav-link" to="/partners">Partenaires</Link>
-                </li>
-                <li className="nav-item">
-                  <Link className="nav-link" to="/prices">Tarifs</Link>
-                </li>
-                <li className="nav-item">
-                  <Link className="nav-link" to="/hives">Les ruches</Link>
-                </li>
-                <div className="dropdown-divider" />
-              </ul>
-            )}
-            {isLoggedIn() ? (
-              <ul className="navbar-nav">
-                <li className="nav-item">
-                  <Link className="nav-link" to="/about">Notre histoire</Link>
-                </li>
-                <li className="nav-item">
-                  <Link className="nav-link" to="/ourvalues">Nos valeurs</Link>
-                </li>
-                <li className="nav-item">
-                  <Link className="nav-link" to="/team">L'équipe</Link>
-                </li>
-                <li className="nav-item">
-                  <Link className="nav-link" to="/contact">Contact</Link>
-                </li>
-                <li className="nav-item">
-                  <Link className="nav-link" to="/jobs">Jobs</Link>
-                </li>
-                <li className="nav-item">
-                  <a className="nav-link" href="https://confidencesdabeilles.fr/" target="_blank" rel="noopener noreferrer">Boutique <FontAwesome name="external-link" /></a>
-                </li>
-                <li className="nav-item">
-                  <a className="nav-link" href="https://confidencesdabeilles.fr/blog" target="_blank" rel="noopener noreferrer">Blog <FontAwesome name="external-link" /></a>
-                </li>
-                <div className="dropdown-divider"></div>
-                <li className="nav-item">
-                  <Link className="nav-link" to="/account">Mon compte</Link>
-                </li>
-                <li className="nav-item">
-                  <Link className="nav-link" to="/logout">Deconnexion</Link>
-                </li>
-              </ul>
-            ) : (
-              <ul className="navbar-nav">
-                <li className="nav-item">
-                  <strong
-                    className="nav-link"
-                    style={{ cursor: 'pointer' }}
-                    data-toggle="modal"
-                    data-target="#newsmodal"
-                  >
-                    <FontAwesome name="envelope-o" /> Newsletters
-                  </strong>
-                </li>
-                <li className="nav-item">
-                  <Link className="nav-link" to="/about">Notre histoire</Link>
-                </li>
-                <li className="nav-item">
-                  <Link className="nav-link" to="/ourvalues">Nos valeurs</Link>
-                </li>
-                <li className="nav-item">
-                  <Link className="nav-link" to="/team">L'équipe</Link>
-                </li>
-                <li className="nav-item">
-                  <Link className="nav-link" to="/contact">Contact</Link>
-                </li>
-                <li className="nav-item">
-                  <Link className="nav-link" to="/jobs">Jobs</Link>
-                </li>
-                <li className="nav-item">
-                  <a className="nav-link" href="https://confidencesdabeilles.fr/" target="_blank" rel="noopener noreferrer">Boutique <FontAwesome name="external-link" /></a>
-                </li>
-                <li className="nav-item">
-                  <a className="nav-link" href="https://confidencesdabeilles.fr/blog" target="_blank" rel="noopener noreferrer">Blog <FontAwesome name="external-link" /></a>
-                </li>
-                <div className="dropdown-divider" />
-                <li className="nav-item">
-                  <Link className="nav-link" to="/login">Se connecter</Link>
-                </li>
-                <li className="nav-item">
-                  <Link className="nav-link" to="/presignup">Créer un compte</Link>
-                </li>
+                  <MyLink className="nav-link" {...props} />
+                </li>)) : (
+                  <Fragment>
+                    {navLinks.mobile.visitors.before.map(props => (
+                      <li className="nav-item">
+                        <MyLink className="nav-link" {...props} />
+                      </li>
+                    ))}
+                    <li className="nav-item">
+                      <strong
+                        className="nav-link"
+                        style={{ cursor: 'pointer' }}
+                        data-toggle="modal"
+                        data-target="#newsmodal"
+                      >
+                        <FontAwesome name="envelope-o" />
+                        &nbsp;
+                        Newsletters
+                      </strong>
+                    </li>
+                    {navLinks.mobile.visitors.after.map(props => (
+                      <li className="nav-item">
+                        <MyLink className="nav-link" {...props} />
+                      </li>
+                    ))}
+                  </Fragment>
+              )}
+            </ul>
+          </div>
+          {isLoggedIn()
+            ? <ul className="navbar-nav hidden-md-down" />
+            : (
+              <ul className="navbar-nav hidden-md-down">
+                {navLinks.desktop.visitors.map(props => (
+                  <li className="nav-item">
+                    <MyLink className="nav-link" {...props} />
+                  </li>
+                ))}
               </ul>
             )}
-      </div>
-        {(isLoggedIn())?
-          <ul className="navbar-nav hidden-md-down">
-
-          </ul>
-          :
-          <ul className="navbar-nav hidden-md-down">
-            <li className="nav-item">
-              <NavLink className="nav-link" to="/company/presentation" activeStyle={{backgroundColor: 'rgb(230,230,230)',
-              boxShadow: '0px 0px 5px 2px rgb(230,230,230)'}}>Entreprise</NavLink>
-            </li>
-            <li className="nav-item">
-              <NavLink className="nav-link" to="/individual/presentation" activeStyle={{backgroundColor: 'rgb(230,230,230)',
-              boxShadow: '0px 0px 5px 2px rgb(230,230,230)'}}>Particulier</NavLink>
-            </li>
-            <li className="nav-item">
-              <NavLink className="nav-link" to="/partners" activeStyle={{backgroundColor: 'rgb(230,230,230)',
-              boxShadow: '0px 0px 5px 2px rgb(230,230,230)'}}>Partenaires</NavLink>
-            </li>
-            <li className="nav-item">
-              <NavLink className="nav-link" to="/prices" activeStyle={{backgroundColor: 'rgb(230,230,230)',
-              boxShadow: '0px 0px 5px 2px rgb(230,230,230)'}}>Tarifs</NavLink>
-            </li>
-            <li className="nav-item">
-              <NavLink className="nav-link" to="/hives" activeStyle={{backgroundColor: 'rgb(230,230,230)',
-              boxShadow: '0px 0px 5px 2px rgb(230,230,230)'}}>Les ruches</NavLink>
-            </li>
-          </ul>
-        }
-        {(isLoggedIn())?
           <ul className="navbar-nav ml-auto hidden-md-down">
+            {!isLoggedIn() && (
+              <li className="nav-item">
+                <strong
+                  className="nav-link"
+                  style={{ cursor: 'pointer' }}
+                  data-toggle="modal"
+                  data-target="#newsmodal"
+                >
+                  <FontAwesome name="envelope-o" />
+                  &nbsp;
+                  Newsletter
+                </strong>
+              </li>
+            )}
             <li className="nav-item dropdown">
               <a className="nav-link dropdown-toggle" style={{ cursor : 'pointer' }} href="http://example.com" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
                 La société
               </a>
               <div className="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
-                <Link className="dropdown-item" to="/about">Notre histoire</Link>
-                <Link className="dropdown-item" to="/ourvalues">Nos valeurs</Link>
-                <Link className="dropdown-item" to="/team">L'équipe</Link>
-                <Link className="dropdown-item" to="/contact">Contact</Link>
-                <Link className="dropdown-item" to="/jobs">Jobs</Link>
-                <a className="dropdown-item" href="https://confidencesdabeilles.fr/" target="_blank" rel="noopener noreferrer">Boutique <FontAwesome name="external-link" /></a>
-                <a className="dropdown-item" href="https://confidencesdabeilles.fr/blog" target="_blank" rel="noopener noreferrer">Blog <FontAwesome name="external-link" /></a>
+                {navLinks.desktop.dropdown.map(props => <MyLink className="dropdown-item" {...props} />)}
               </div>
             </li>
-            <li className="nav-item">
-              &nbsp;&nbsp;<Link className="btn btn-primary" to="/account">Mon compte</Link>
-          </li>
-          <li className="nav-item">
-            &nbsp;&nbsp;<Link className="btn btn-primary" to="/logout">Deconnexion</Link>
-        </li>
-      </ul>
-      :
-      <ul className="navbar-nav ml-auto hidden-md-down">
-        <li className="nav-item">
-          <strong className="nav-link"
-            style={{ cursor : 'pointer' }}
-            data-toggle="modal" data-target="#newsmodal">
-            <FontAwesome name="envelope-o" /> Newsletter
-          </strong>
-        </li>
-        <li className="nav-item dropdown">
-          <span className="nav-link dropdown-toggle" style={{ cursor : 'pointer' }} id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-            La société
-          </span>
-          <div className="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
-            <Link className="dropdown-item" to="/about">Notre histoire</Link>
-            <Link className="dropdown-item" to="/ourvalues">Nos valeurs</Link>
-            <Link className="dropdown-item" to="/team">L'équipe</Link>
-            <Link className="dropdown-item" to="/contact">Contact</Link>
-            <Link className="dropdown-item" to="/jobs">Jobs</Link>
-            <a className="dropdown-item" href="https://confidencesdabeilles.fr/" target="_blank" rel="noopener noreferrer">Boutique <FontAwesome name="external-link" /></a>
-            <a className="dropdown-item" href="https://confidencesdabeilles.fr/blog" target="_blank" rel="noopener noreferrer">Blog <FontAwesome name="external-link" /></a>
-          </div>
-        </li>
-
-        <li className="nav-item">
-          &nbsp;&nbsp;<Link className="btn btn-primary" to="/login">Se connecter</Link>
-        </li>
-        <li className="nav-item">
-          &nbsp;&nbsp;<Link className="btn btn-primary" to="/presignup">Créer un compte</Link>
-        </li>
-
-
-  </ul>
+            {isLoggedIn()
+              ? (
+                <Fragment>
+                  <li className="nav-item">
+                    &nbsp;&nbsp;
+                    <Link className="btn btn-primary" to="/account">Mon compte</Link>
+                  </li>
+                  <li className="nav-item">
+                    &nbsp;&nbsp;
+                    <Link className="btn btn-primary" to="/logout">Deconnexion</Link>
+                  </li>
+                </Fragment>
+              ) : (
+                <Fragment>
+                  <li className="nav-item">
+                    &nbsp;&nbsp;
+                    <Link className="btn btn-primary" to="/login">Se connecter</Link>
+                  </li>
+                  <li className="nav-item">
+                    &nbsp;&nbsp;
+                    <Link className="btn btn-primary" to="/presignup">Créer un compte</Link>
+                  </li>
+                </Fragment>
+              )}
+          </ul>
+          {this.newsletterPopup()}
+        </nav>
+      </div>
+    );
   }
-  {this.newsletterPopup()}
-  </nav>
-</div>
-);
-}
 }
