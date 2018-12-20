8101c71a9b6434fb51f112f6086d9f79092e9762
Clément CHAMPOUILLON <clement.champouillon@link-value.fr>
2018-12-20 23:41:11 +0100

    refactor(Loading)

---
+++ src/components/utils/Loading.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import loader from '../../assets/img/three-dots.svg';
+
+export default () => (
+  <div style={{ width: '100%', textAlign: 'center', height: '60px', paddingTop: '20px' }}>
+    <img src={loader} alt="Loading" />
+  </div>
+);
