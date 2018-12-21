8e7436534b3d036dd9cc2315bdb6cd9d64af2c84
Clément CHAMPOUILLON <clement.champouillon@link-value.fr>
2018-12-21 09:43:37 +0100

    refactor(Router)

--- src/components/Present.jsx
+++ src/components/Present.jsx
@@ -5,28 +5,31 @@ import Meta from './utils/Meta';
 
 export default () => (
   <div className="container">
-    <Meta title="Offrir un parrainage"/>
+    <Meta title="Offrir un parrainage" />
     <div className="row mb-4">
-        <div className="col">
-            <img className="img-fluid" src={presentPict} alt="Bannière cadeau" />
-        </div>
+      <div className="col">
+        <img className="img-fluid" src={presentPict} alt="Bannière cadeau" />
+      </div>
     </div>
     <div className="row">
-        <div className="col">
-            <h2 className="text-center my-4">Parrainer des abeilles : un cadeau aussi utile qu’original !</h2>
-        </div>
+      <div className="col">
+        <h2 className="text-center my-4">Parrainer des abeilles : un cadeau aussi utile qu’original !</h2>
+      </div>
     </div>
     <div className="row justify-content-center">
-        <div className="col-lg-9 col-md-10 col-sm-12">
-            <p className="mb-5">
-                <strong>Vous cherchiez un cadeau original qu’on puisse à la fois toucher du regard, expérimenter, déguster et qui soit
-                concrètement bénéfique pour notre
-                environnement ?</strong> Alors vous êtes au bon
-                endroit ! En effet, le parrainage d’Abeilles ou
-                de ruches a la particularité de réunir tous ces
-                atouts !
-            </p>
-        </div>
+      <div className="col-lg-9 col-md-10 col-sm-12">
+        <p className="mb-5">
+          <strong>
+            Vous cherchiez un cadeau original qu’on puisse à la fois toucher du regard, expérimenter, déguster et qui soit
+            concrètement bénéfique pour notre
+            environnement ?
+          </strong>
+          Alors vous êtes au bon
+        endroit ! En effet, le parrainage d’Abeilles ou
+        de ruches a la particularité de réunir tous ces
+        atouts !
+          </p>
+      </div>
     </div>
     <div className="row justify-content-center">
       <div className="col-lg-9 col-md-10 col-sm-12">
@@ -38,7 +41,7 @@ export default () => (
             <a className="nav-link lead" data-toggle="tab" href="#presentation">Offrir un parrainage, c’est quoi ?</a>
           </li>
         </ul>
-        <div className="tab-content" style={{ overflowX : 'auto' }}>
+        <div className="tab-content" style={{ overflowX: 'auto' }}>
           <div id="how-offer" className="tab-pane active" role="tabpanel">
             <HowOfferPresent />
           </div>
@@ -53,52 +56,52 @@ export default () => (
 const HowOfferPresent = () => (
   <div className="container mt-4">
     <div className="row">
-        <div className="col">
-          <p className="">
-              Dans le but de garantir une bonne saisie des
+      <div className="col">
+        <p className="">
+          Dans le but de garantir une bonne saisie des
               informations <strong>nous vous invitons à lire toutes
               ces étapes avant de commencer.</strong>
-          </p>
-          <ol className="lead">
-              <li className="my-4">
-                  Inscrivez vous avec vos propres
-                  informations (elles sont nécessaires à la
-                  gestion de votre compte / à la
-                  facturation)
+        </p>
+        <ol className="lead">
+          <li className="my-4">
+            Inscrivez vous avec vos propres
+            informations (elles sont nécessaires à la
+            gestion de votre compte / à la
+            facturation)
               </li>
-              <li className="my-4">
-                  Choisissez un nombre d’abeilles à offrir
+          <li className="my-4">
+            Choisissez un nombre d’abeilles à offrir
               </li>
-              <li className="my-4">
-                  Désignez votre bénéficiaire en cochant
-                  « Ce parrainage est un cadeau » et en
-                  renseignant ses coordonnées.
+          <li className="my-4">
+            Désignez votre bénéficiaire en cochant
+            « Ce parrainage est un cadeau » et en
+            renseignant ses coordonnées.
 
               </li>
-              <li className="my-4">
-              ATTENTION à la date à partir de
+          <li className="my-4">
+            ATTENTION à la date à partir de
               laquelle doit être notifié le bénéficiaire.<br /><strong>Un délai de 3 jours est recommandé.</strong>
+          </li>
+          <li className="my-4">
+            Terminez en réglant votre cadeau (la
+            confirmation de règlement est
+            nécessaire pour la génération du
+            parrainage)
               </li>
-              <li className="my-4">
-                  Terminez en réglant votre cadeau (la
-                  confirmation de règlement est
-                  nécessaire pour la génération du
-                  parrainage)
-              </li>
-              <li className="my-4">
-                  <span className="align-middle">C’est terminé </span>
-                  <img src={require('../assets/img/smiley/happy.svg')} alt="smiley happy"
-                    style={{ height: '1em' }} />
-              </li>
-          </ol>
-        </div>
+          <li className="my-4">
+            <span className="align-middle">C’est terminé </span>
+            <img src={require('../assets/img/smiley/happy.svg')} alt="smiley happy"
+              style={{ height: '1em' }} />
+          </li>
+        </ol>
       </div>
-      <div className="row">
-        <div className="col">
-          <p className="text-center">
-              <Link to="/signup/individual" className="btn btn-secondary">J'offre un parrainage<br/>d’abeilles</Link>
-          </p>
-        </div>
+    </div>
+    <div className="row">
+      <div className="col">
+        <p className="text-center">
+          <Link to="/signup/individual" className="btn btn-secondary">J'offre un parrainage<br />d’abeilles</Link>
+        </p>
+      </div>
     </div>
   </div>
 );
@@ -106,36 +109,36 @@ const HowOfferPresent = () => (
 const PresentationPresent = () => (
   <div className="container mt-4">
     <div className="row">
-        <div className="col">
-          <p>
-            Votre cadeau permet à l’heureux bénéficiaire de devenir parrain de 10 000 abeilles (ou
-            plus). 10 000, tant que ça ?! Eh oui, il faut savoir qu’une ruche compte en moyenne 50 000
-            individus. Des informations comme celle-ci mais aussi des photos de leurs abeilles, les
-            heureux parrains en recevront tout au long de l’année que dure leur parrainage. Il y a
-            beaucoup à apprendre sur ce monde fascinant et plus captivant encore. Ils auront aussi la
-            possibilité de prendre part aux visites des ruches au cours de l’année ! L’occasion pour eux
-            d’enfiler la fameuse combinaison de l’apiculteur et de plonger littéralement au cœur de la
-            colonie !
+      <div className="col">
+        <p>
+          Votre cadeau permet à l’heureux bénéficiaire de devenir parrain de 10 000 abeilles (ou
+          plus). 10 000, tant que ça ?! Eh oui, il faut savoir qu’une ruche compte en moyenne 50 000
+          individus. Des informations comme celle-ci mais aussi des photos de leurs abeilles, les
+          heureux parrains en recevront tout au long de l’année que dure leur parrainage. Il y a
+          beaucoup à apprendre sur ce monde fascinant et plus captivant encore. Ils auront aussi la
+          possibilité de prendre part aux visites des ruches au cours de l’année ! L’occasion pour eux
+          d’enfiler la fameuse combinaison de l’apiculteur et de plonger littéralement au cœur de la
+          colonie !
           </p>
-          <p>
-            Enfin, après avoir bien travaillé, les abeilles partageront une partie de leur trésor pour le
-            plus grand plaisir des petits et des grands. Un moment gastronomique inoubliable.
+        <p>
+          Enfin, après avoir bien travaillé, les abeilles partageront une partie de leur trésor pour le
+          plus grand plaisir des petits et des grands. Un moment gastronomique inoubliable.
           </p>
-          <p>
-            <strong>Par votre cadeau, les parrains prennent plaisir à suivre l’évolution de leur ruche via une
+        <p>
+          <strong>Par votre cadeau, les parrains prennent plaisir à suivre l’évolution de leur ruche via une
             page dédiée et participent également à la sauvegarde d’un métier et d’une espèce menacée.</strong><br />
-            Les abeilles, par leur activité de pollinisation, protègent la biodiversité et préservent donc
-            notre environnement. Celui-ci étant plus fécond il nécessite moins de traitements
-            nuisibles aux abeilles. C’est un cercle vertueux qu’il faut réussir à amorcer !
+          Les abeilles, par leur activité de pollinisation, protègent la biodiversité et préservent donc
+          notre environnement. Celui-ci étant plus fécond il nécessite moins de traitements
+          nuisibles aux abeilles. C’est un cercle vertueux qu’il faut réussir à amorcer !
           </p>
-        </div>
+      </div>
     </div>
     <div className="row">
-        <div className="col">
-          <p className="text-center" >
-              <Link to="/individual/more" className="btn btn-secondary">En savoir plus</Link>
-          </p>
-        </div>
+      <div className="col">
+        <p className="text-center" >
+          <Link to="/individual/more" className="btn btn-secondary">En savoir plus</Link>
+        </p>
+      </div>
     </div>
   </div>
 );
