027c0091cc67918f6cb480e9a9f70f382f1f0358
c-champouillon <cchampouillon.externe@m6.fr>
2019-03-26 21:14:03 +0100

    sentry

--- src/index.jsx
+++ src/index.jsx
@@ -1,5 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import * as Sentry from '@sentry/browser';
+
+Sentry.init({
+  dsn: 'https://5d9a3bd8d34a44e9b6091409f38392e1@sentry.io/1412093',
+});
+
 
 ReactDOM.render(<App />, document.getElementById('root'));
