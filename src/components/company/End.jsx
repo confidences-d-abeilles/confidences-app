c529cc17aa16c1bee396765368438b2f0a7a4364
c-champouillon <cchampouillon.externe@m6.fr>
2019-04-02 21:10:44 +0200

    End screen company

--- src/components/company/End.jsx
+++ src/components/company/End.jsx
@@ -1,87 +1,60 @@
 import React, { Component } from 'react';
 import { Redirect, Link } from 'react-router-dom';
+import NotificationSystem from 'react-notification-system';
 import Main from '../../assets/img/end_part.jpg';
 
-import Meta from '../utils/Meta'
+import Meta from '../utils/Meta';
 import request from '../../services/Net';
-import NotificationSystem from 'react-notification-system'
-import Loading from '../utils/Loading'
+import Loading from '../utils/Loading';
+import PayChecker from '../utils/PayChecker';
 
 export default class CompanyEnd extends Component {
-
-  constructor(props) {
-    super(props)
-
-    this.state = {
-      namespace: '',
-      loading: false
-    }
-  }
+  state = {
+    namespace: '',
+    loading: false,
+    bundleId: null,
+  };
 
   componentDidMount() {
     this.setState({ loading: true });
     request({
       url: '/user/me',
-      method: 'get'
+      method: 'get',
     }, this.refs.notif)
       .then((res) => {
         this.setState({
           namespace: res.namespace,
-          firstname: res.firstname,
-          name: res.name,
-          bundleState: res.bundles[0].state,
-          loading: false
+          bundleId: res.bundles[0].id,
         });
-        // request({
-        // 	url: '/newsletter/create',
-        // 	method: 'put',
-        // 	data: {
-        // 		firstname: user.firstname,
-        // 		email: user.email,
-        // 		id: user.id
-        // 	}
-        // }).then((res) => {
-        // 	console.log('good');
-        // })
-        setTimeout(() => { this.setState({ redirecte: true }) }, 8000);
       });
   }
 
   render() {
+    const { bundleId, namespace } = this.state;
     return (
       <div className="container py-4">
         <Meta title="Félicitations" />
         <NotificationSystem ref="notif" />
-        {this.state.redirecte ? <Redirect to="/company/manage" /> : null}
-        <div className="row justify-content-center">
-          {(this.state.loading) ?
-            <div className="col-8 text-center">
-              <h3>En attente de confirmation</h3>
-              <Loading />
-            </div>
-            : <div className="col-8">
-              {!this.state.bundleState ? <h2 className="text-center my-4">Génial ! Vous avez choisi de rejoindre notre aventure.</h2>
-                : <h2 className="text-center my-4">Félicitations ! Vous faites désormais partie de la grande famille des parrains de ruches.</h2>
-              }
+        <PayChecker bundleId={bundleId}>
+          <div className="row justify-content-center">
+            <div className="col-8">
+              <h2 className="text-center my-4">Génial ! Vous avez choisi de rejoindre notre aventure.</h2>
               <p className="text-center">
                 <img src={Main} className="img-fluid mx-auto d-block" alt="Img temp" />
                 <br />
-                {!this.state.bundleState ?
-                  <h4 className="text-center my-4">Toute l'équipe de Confidences d'Abeilles vous souhaite la bienvenue.</h4>
-                  : <h4 className="text-center my-4">Toute l'équipe de Confidences  d'Abeilles vous remercie !</h4>
-                }
+                <h4 className="text-center my-4">Toute l'équipe de Confidences  d'Abeilles vous remercie !</h4>
               </p>
               <div className="row justify-content-center">
                 <div className="col text-center">
-                  <Link to="/company/manage" className="btn btn-primary btn-lg">{!this.state.bundleState ? 'Découvrir mon espace' : 'Mon compte'}</Link>
+                  <Link to="/company/manage" className="btn btn-primary btn-lg">Mon compte</Link>
                 </div>
                 <div className="col text-center">
-                  <Link to={'/parrains/' + this.state.namespace} className="btn btn-primary btn-lg">Découvrir notre page</Link>
+                  <Link to={`/parrains/${namespace}`} className="btn btn-primary btn-lg">Découvrir notre page</Link>
                 </div>
               </div>
             </div>
-          }
-        </div>
+          </div>
+        </PayChecker>
       </div>
     );
   }
