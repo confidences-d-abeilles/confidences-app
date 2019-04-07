a9dd62daf43056c331a2e407debe0956b30c7085
c-champouillon <cchampouillon.externe@m6.fr>
2019-04-07 15:33:59 +0200

    sentry env

--- src/index.jsx
+++ src/index.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
 import * as Sentry from '@sentry/browser';
+
+import App from './App';
 
 Sentry.init({
   dsn: 'https://5d9a3bd8d34a44e9b6091409f38392e1@sentry.io/1412093',
+  environment: process.env.NODE_ENV,
 });
 
 ReactDOM.render(<App />, document.getElementById('root'));
