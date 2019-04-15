e980dcf4148b078e2e04713c66719ac0de9319b3
cchampou <clement@champouillon.com>
2019-04-15 22:03:57 +0200

    photo

--- src/components/admin/manage/hives/Board.jsx
+++ src/components/admin/manage/hives/Board.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect, PureComponent } from 'react';
 import { Link } from 'react-router-dom';
 import PropTypes from 'prop-types';
 
@@ -13,29 +13,40 @@ import FileUpload from '../../../utils/FileUpload';
 import { Button } from '../../../utils/Button';
 import Pictures from './Pictures';
 
-const Board = ({ notification, match: { params: { hiveId } } }) => {
-  const [hive, setHive] = useState(null);
-  const [newsToEdit, setNewsToEdit] = useState(null);
-  const get = () => {
+class Board extends PureComponent {
+  state = {
+    hive: null,
+    newsToEdit: null,
+  };
+
+  componentDidMount() {
+    this.get();
+  }
+
+  get = () => {
+    const { notification, match: { params: { hiveId } } } = this.props;
     request({
       url: `/hive/${hiveId}`,
       method: 'get',
-    }, notification).then(res => setHive(res));
-  };
+    }, notification)
+      .then(res => this.setState({ hive: res }));
+  }
 
-  const updateInfo = (name, value) => (e) => {
+  updateInfo = (name, value) => (e) => {
     if (e) { e.preventDefault(); }
+    const { notification, match: { params: { hiveId } } } = this.props;
     request({
       url: `/hive/${hiveId}`,
       method: 'patch',
       data: {
         [name]: value,
       },
-    }, notification).then(() => setHive({ ...hive, [name]: value }));
+    }, notification).then(() => this.setState({ hive: { ...this.state.hive, [name]: value } }));
   };
 
-  const addPhoto = (e) => {
+  addPhoto = (e) => {
     e.preventDefault();
+    const { notification, match: { params: { hiveId } } } = this.props;
     const data = new FormData();
     data.append('id', hiveId);
     if (document.getElementById('hive-img').files[0]) {
@@ -47,53 +58,56 @@ const Board = ({ notification, match: { params: { hiveId } } }) => {
         header: {
           'content-type': 'multipart/form-data',
         },
-      }, notification).then(() => get());
+      }, notification).then(() => this.get());
     }
   };
 
-  if (!hive) {
-    get();
-    return <Loading />;
-  }
 
-  return (
-    <>
-      <div className="row">
-        <div className="col">
-          <ol className="breadcrumb">
-            <li className="breadcrumb-item"><Link to="/admin/manage">Panel d'Administration</Link></li>
-            <li className="breadcrumb-item"><Link to="/admin/manage/hives">Ruches</Link></li>
-            <li className="breadcrumb-item active">{hive && hive.name}</li>
-          </ol>
+  render() {
+    const { newsToEdit, hive } = this.state;
+    const { notification, match: { params: { hiveId } } } = this.props;
+    if (!this.state.hive) {
+      return <Loading />;
+    }
+    return (
+      <>
+        <div className="row">
+          <div className="col">
+            <ol className="breadcrumb">
+              <li className="breadcrumb-item"><Link to="/admin/manage">Panel d'Administration</Link></li>
+              <li className="breadcrumb-item"><Link to="/admin/manage/hives">Ruches</Link></li>
+              <li className="breadcrumb-item active">{hive && hive.name}</li>
+            </ol>
+          </div>
         </div>
-      </div>
-      <div className="row">
-        <div className="col-lg-4">
-          <Parrains parrainsList={(hive && hive.parrains) || []} />
-          <Rating value={(hive && parseFloat(hive.ratio)) || 2.5} handler={(value) => updateInfo('ratio', value)()} />
-          <h2>Informations</h2>
-          <Info name="info" handler={updateInfo} defaultValue={hive.info} />
-          <h2>Mémo technique</h2>
-          <Info name="feedback" handler={updateInfo} defaultValue={hive.feedback} />
+        <div className="row">
+          <div className="col-lg-4">
+            <Parrains parrainsList={(hive && hive.parrains) || []} />
+            <Rating value={(hive && parseFloat(hive.ratio)) || 2.5} handler={value => this.updateInfo('ratio', value)()} />
+            <h2>Informations</h2>
+            <Info name="info" handler={this.updateInfo} defaultValue={hive.info} />
+            <h2>Mémo technique</h2>
+            <Info name="feedback" handler={this.updateInfo} defaultValue={hive.feedback} />
+          </div>
+          <div className="col-lg-8">
+            <h2>Actualités</h2>
+            <select onChange={({ target: { value: toEdit } }) => this.setState({ newsToEdit: toEdit })} className="my-2">
+              <option value={null}>Choisissez une actualité à modifier</option>
+              {hive.news.map(news => <option value={news.id} key={news.id}>{news.title}</option>)}
+            </select>
+            <Feedback name={this.state.newsToEdit} hiveId={hiveId} />
+            <h2>Photos</h2>
+            <form onSubmit={this.addPhoto}>
+              <FileUpload label="Taille recommandé : 400 x 300" identifier="hive-img" />
+              <Button type="submit" primary>Ajouter</Button>
+            </form>
+            <Pictures data={hive.imgs} hiveId={hiveId} refresh={this.get} />
+          </div>
         </div>
-        <div className="col-lg-8">
-          <h2>Actualités</h2>
-          <select onChange={({ target: { value: toEdit } }) => setNewsToEdit(toEdit)} className="my-2">
-            <option value={null}>Choisissez une actualité à modifier</option>
-            {hive.news.map(news => <option value={news.id} key={news.id}>{news.title}</option>)}
-          </select>
-          <Feedback name={newsToEdit} hiveId={hiveId} />
-          <h2>Photos</h2>
-          <form onSubmit={addPhoto}>
-            <FileUpload label="Taille recommandé : 400 x 300" identifier="hive-img" />
-            <Button type="submit" primary>Ajouter</Button>
-          </form>
-          <Pictures data={hive.imgs} hiveId={hiveId} refresh={get()} />
-        </div>
-      </div>
-    </>
-  );
-};
+      </>
+    );
+  }
+}
 
 Board.propTypes = {
   match: PropTypes.shape({
--- src/components/admin/manage/hives/Pictures.jsx
+++ src/components/admin/manage/hives/Pictures.jsx
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 
 import request from '../../../../services/Net';
 import { withNotification } from '../../../../services/withNotification';
+import { Button } from '../../../utils/Button';
 
 export default withNotification(class Pictures extends Component {
 
@@ -15,14 +16,27 @@ export default withNotification(class Pictures extends Component {
     })
   }
 
+  setAsDefault = index => {
+    const { notification } = this.props;
+    request({
+      url : '/hive/img/'+this.props.hiveId,
+      method: 'put'
+    }, notification).then(res => {
+      this.props.refresh();
+    })
+  }
+
   render () {
     return (
       <div className="mt-4">
         {(this.props.data && this.props.data.length !== 0)?
           <div>
-            <p className="text-center"><small><em>Cliquez sur une des images pour la supprimer instantanément</em></small></p>
             {this.props.data.map((img, index) => (
-              <img key={index} alt={img} className="w-25 p-2" src={process.env.REACT_APP_CONTENT_DOMAIN+'/'+img} onClick={this.delete.bind(this, index)} style={{ cursor: 'pointer' }} />
+              <>
+                <img key={index} alt={img} className="w-25 p-2" src={process.env.REACT_APP_CONTENT_DOMAIN+'/'+img} style={{ cursor: 'pointer' }} />
+                <Button onClick={this.delete.bind(this, index)}>Supprimer</Button>
+                <Button onClick={this.setAsDefault.bind(this, index)}>Par defaut</Button>
+              </>
             ))}
           </div>
         :<p>Aucune image à afficher pour cette ruche</p>}
