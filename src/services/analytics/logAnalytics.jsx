8e23f2e4b1e21fd0f42db4405ee3b23f3b21e2be
cchampou <clement@champouillon.com>
2019-04-15 23:05:29 +0200

    pixel

--- src/services/analytics/logAnalytics.jsx
+++ src/services/analytics/logAnalytics.jsx
@@ -1,8 +1,10 @@
 import React from 'react';
 import ReactGA from 'react-ga';
+import ReactPixel from 'react-facebook-pixel';
 
 export default BaseComponent => (props) => {
   const { location: { pathname } } = props;
   ReactGA.pageview(pathname);
+  ReactPixel.pageView();
   return <BaseComponent {...props} />;
 };
