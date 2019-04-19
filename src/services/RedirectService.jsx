0871b33b390bc56c60b367819aa4168fa11b550e
c-champouillon <cchampouillon.externe@m6.fr>
2019-04-19 18:24:33 +0200

    fixed some errors and added i18n

--- src/services/RedirectService.jsx
+++ src/services/RedirectService.jsx
@@ -1,7 +1,4 @@
-
 import React from 'react';
 import { Redirect } from 'react-router-dom';
 
-export function redirect(route) {
-    return <Redirect to={route} />;
-}
+export default route => <Redirect to={route} />;
