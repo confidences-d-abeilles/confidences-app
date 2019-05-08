ae2f42b9fa6c1e7fc44d3b426c008d137c182cba
cchampou <clement@champouillon.com>
2019-05-08 15:08:20 +0200

    nofooter

--- src/services/PublicRouter.jsx
+++ src/services/PublicRouter.jsx
@@ -170,11 +170,6 @@ const Logout = Loadable({
   loading: () => null,
 });
 
-const Aviva = Loadable({
-  loader: () => import('../modules/aviva/Aviva.jsx'),
-  loading: () => null,
-});
-
 export default () => (
   <Switch>
     <Route exact path="/" component={Home} />
@@ -210,7 +205,6 @@ export default () => (
     <Route exact path="/confirm/:token" component={Confirm} />
     <Route exact path="/logout" component={Logout} />
     <Route exact path="/account" component={MyAccount} />
-    <Route exact path="/la-fabrique-aviva-projet-confidences-abeilles-concours" component={Aviva} />
     <Route component={NotFound} />
   </Switch>
 );
