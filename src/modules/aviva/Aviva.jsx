ffd942c91f3dfa133476e1e23e7c52c8b153801f
cchampou <clement@champouillon.com>
2019-05-08 14:16:19 +0200

    aviva

---
+++ src/modules/aviva/Aviva.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import { ButtonLink } from '../../components/utils/Button';
+
+export default () => (
+  <div className="container">
+    <h1>Aidez-nous à remporter le concours AVIVA votez pour notre projet et gagnez un séjour à Annecy 2 bons d’achat de 50€</h1>
+    <p className="text-center">
+      <ButtonLink href="https://lafabriqueaviva.fr/fr/project/1105/show" primary external>Voter</ButtonLink>
+    </p>
+    <h2>Un mot sur le projet</h2>
+    <p>Renouer avec la nature, s'engager pour la biodiversité, protéger les abeilles, préserver le terroir, le savoir-faire français et consommer de la qualité, voilà nos ambitions. Grâce au concours AVIVA et ses généreuses subventions, nos abeilles pourraient enfin aspirer à polliniser toutes les fleurs de France</p>
+    <h2>Comment gagner un séjour à Annecy ?</h2>
+    <ul>
+      <li>Cliquez sur le bouton «Je vote» ci-dessus</li>
+      <li>Créez un compte pour pouvoir voter (15 secondes avec Facebook)</li>
+      <li>Attribuez vos 10 votes à notre projet</li>
+      <li>Rendez-vous sur notre publication Facebook, likez et invitez en commentaire la personne avec qui vous voudriez passer ce séjour à nous soutenir également</li>
+    </ul>
+    <p>Le 15 juin, nous tirerons les 3 gagnants parmi les personnes respectant les conditions ci-dessus.</p>
+    <h3>3 lots à gagner : unséjour à Annecy avec Alpes Bivouac et 2 bons d’achat de 50€ à valoir sur confidencesdabeilles.fr</h3>
+    <p>Un grand merci pour votre aide ! L’équipe et les abeilles</p>
+  </div>
+);
