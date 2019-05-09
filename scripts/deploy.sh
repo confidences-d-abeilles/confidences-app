a33de17337abec05976e3821112d0206697d11b5
c-champouillon <cchampouillon.externe@m6.fr>
2019-05-09 22:38:30 +0200

    fix

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,6 +1,6 @@
 start=$SECONDS
-# curl -X POST -H 'Content-type: application/json' --data '{"text":":warning: Deploying new release"}' https://hooks.slack.com/services/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
-rsync -r -v -u -e "ssh -p 1023" ./build/. dev@82.165.69.217:/srv/confidences-app/build/.
+curl -X POST -H 'Content-type: application/json' --data '{"text":":warning: Deploying new release"}' https://hooks.slack.com/services/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
+rsync -r -z -v -u -e "ssh -p 1023" ./build/. dev@82.165.69.217:/srv/confidences-app/build/.
 ssh dev@82.165.69.217 -p 1023 "/home/dev/.nvm/versions/node/v10.10.0/bin/pm2 reload app"
-# curl -X POST -H 'Content-type: application/json' --data '{"text":":recycle: Release complete !"}' https://hooks.slack.com/services/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
 duration=$(( SECONDS - start ))
+curl -X POST -H 'Content-type: application/json' --data '{"text":":recycle: Release complete !"}' https://hooks.slack.com/services/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
