1c0129579bd86284717b41312ac5b43f273e2c0b
c-champouillon <cchampouillon.externe@m6.fr>
2019-05-09 21:06:01 +0200

    lint fix

--- src/components/admin/manage/products/List.jsx
+++ src/components/admin/manage/products/List.jsx
@@ -6,7 +6,7 @@ import { withNotification } from '../../../../services/withNotification';
 
 export default withNotification(class List extends Component {
   state = {
-    products : [],
+    products: [],
     loading: true,
   };
 
@@ -18,80 +18,84 @@ export default withNotification(class List extends Component {
     this.refresh();
   }
 
-  refresh() {
+  refresh = () => {
     const { notification } = this.props;
     this.setState({
-      loading: true
+      loading: true,
     });
     request({
       url: '/product',
-      method: 'get'
+      method: 'get',
     }, notification).then((res) => {
       this.setState({
         products: res,
-        loading: false
+        loading: false,
       });
-    }).catch((e) => {
+    }).catch(() => {
       this.setState({
-        loading: false
-      })
-    })
-  }
+        loading: false,
+      });
+    });
+  };
 
   delete = (id) => {
     const { notification } = this.props;
     request({
-      url: '/product/'+id,
-      method: 'delete'
-    }, notification).then((res) => {
-      let newProducts = this.state.products.filter((product) => {
-        return product.id !== id;
-      });
+      url: `/product/${id}`,
+      method: 'delete',
+    }, notification).then(() => {
+      const { products } = this.state;
+      const newProducts = products.filter(product => product.id !== id);
       this.setState({
-        products: newProducts
+        products: newProducts,
       });
-    }).catch((e) => {
+    }).catch(() => {
       this.refresh();
-    })
-  }
+    });
+  };
 
-  decode(type) {
+  decode = (type) => {
     switch (type) {
       case 10:
-        return ("Parrainage entreprise");
+        return ('Parrainage entreprise');
       case 11:
-        return ("Produit suplémentaire entreprise");
+        return ('Produit suplémentaire entreprise');
       case 20:
-        return ("Parrainage particulier");
+        return ('Parrainage particulier');
       case 21:
-        return ("Produit suplémentaire particulier");
+        return ('Produit suplémentaire particulier');
       default:
         return null;
     }
-  }
+  };
 
-  render () {
+  render() {
+    const { products, loading } = this.state;
     return (
       <div>
         <table className="table table-sm">
           <tbody>
-            <tr><th>Designation</th><th>Type</th><th>Prix HT</th><th>TVA</th><th>Prix TTC</th></tr>
-            {this.state.products.map((product) => {
-              return (
-                <tr>
-                  <td>{product.designation}</td>
-                  <td>{this.decode(product.type)}</td>
-                  <td>{product.price} €</td>
-                  <td>{product.duty} %</td>
-                  <td>{(product.price + (product.price / 100 * product.duty)).toFixed(2)} €</td>
-                  <td><button className="btn btn-primary btn-sm" onClick={this.delete.bind(this, product.id)}>Supprimer</button></td>
-                </tr>
-              );
-            })}
+            <tr>
+              <th>Designation</th>
+              <th>Type</th>
+              <th>Prix HT</th>
+              <th>TVA</th>
+              <th>Prix TTC</th>
+            </tr>
+            {products.map(product => (
+              <tr>
+                <td>{product.designation}</td>
+                <td>{this.decode(product.type)}</td>
+                <td>{`${product.price} €`}</td>
+                <td>{`${product.duty} %`}</td>
+                <td>{`${(product.price + (product.price / 100 * product.duty)).toFixed(2)} €`}</td>
+                <td><button className="btn btn-primary btn-sm" type="button" onClick={this.delete(product.id)}>Supprimer</button></td>
+              </tr>
+            ))}
           </tbody>
         </table>
-        {this.state.loading && <Loading />}
+        {loading && <Loading />}
       </div>
-    )
+    );
   }
 });
