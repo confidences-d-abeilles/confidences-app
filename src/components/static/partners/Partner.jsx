8461f6ec135f69eb03ab4f9441e3f95d0409f370
c-champouillon <cchampouillon.externe@m6.fr>
2019-05-14 20:57:56 +0200

    static improvments

--- src/components/static/partners/Partner.jsx
+++ src/components/static/partners/Partner.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const Partner = props => (
 	<div className="row mb-5">
 		<div className="col-lg-4 text-center d-flex flex-column justify-content-center py-5">
-			<img src={props.img} alt="Thomas Apiculture" className="img-fluid align-self-center" style={{ maxHeight : "10em", flex: 0 }} />
+			<img src={props.img} alt="Thomas Apiculture" className="img-fluid align-self-center" style={{ maxHeight : "10em" }} />
 		</div>
 		<div className="col-lg-8" style={{ fontFamily : "highTo", fontSize : "1.25em" }}>
 			<h3 className="mb-4" style={{ color : "#E49C00" }} >{props.title}</h3>
