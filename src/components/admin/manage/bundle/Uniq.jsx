56c282452a0b5ed5508ab0fa59dda8b814826f59
c-champouillon <cchampouillon.externe@m6.fr>
2019-06-15 13:52:10 +0200

    udpted

--- src/components/admin/manage/bundle/Uniq.jsx
+++ src/components/admin/manage/bundle/Uniq.jsx
@@ -10,9 +10,9 @@ import Parrains from './tiles/Parrains';
 import Label from './tiles/Label';
 import Bundle from '../users/tiles/Bundle';
 import { withNotification } from '../../../../services/withNotification';
+import Price from './tiles/Price';
 
 export default withNotification(class AdminManageBundleId extends Component {
-
   state = {
     bundle: null,
     parrain: null,
@@ -23,39 +23,39 @@ export default withNotification(class AdminManageBundleId extends Component {
     const { notification } = this.props;
     request({
       url: `/bundle/${this.props.match.params.id}`,
-      method: 'get'
+      method: 'get',
     }, notification).then((res) => {
       this.setState({
-        bundle: { ...res, state : res.state.toString() },
-        owner: res.owner
+        bundle: { ...res, state: res.state.toString() },
+        owner: res.owner,
       });
 
       request({
         method: 'get',
-        url: '/user/' + res.owner.id,
+        url: `/user/${res.owner.id}`,
       }, notification).then((res2) => {
         this.setState({
           userLabelFilename: res2.label,
-          loading: false
+          loading: false,
         });
-      })
-    })
+      });
+    });
   }
 
-  changeState = ( event ) => {
+  changeState = (event) => {
     this.setState({
-      bundle: { ...this.state.bundle, state: parseInt(event.target.value, 10) }
-    })
+      bundle: { ...this.state.bundle, state: parseInt(event.target.value, 10) },
+    });
   }
 
   submitState = () => {
     const { notification } = this.props;
     request({
-      url: '/bundle/'+this.state.bundle.id,
+      url: `/bundle/${this.state.bundle.id}`,
       method: 'put',
       data: {
-        state : this.state.bundle.state
-      }
+        state: this.state.bundle.state,
+      },
     }, notification).then((res) => {
 
     });
@@ -64,61 +64,66 @@ export default withNotification(class AdminManageBundleId extends Component {
   downloadLabel = async () => {
     const { notification } = this.props;
     try {
-      let response = await client({
-        url: '/bundle/'+this.state.bundle.id+'/label',
+      const response = await client({
+        url: `/bundle/${this.state.bundle.id}/label`,
         method: 'GET',
         responseType: 'arraybuffer',
       });
       let retrievedFileName;
       try {
-        let regexp = /filename="(.*)"/gi;
+        const regexp = /filename="(.*)"/gi;
         retrievedFileName = regexp.exec(response.headers['content-disposition'])[1];
       } catch (err) {
         console.log('erreur filename');
-        retrievedFileName = 'label_' + this.state.bundle.owner.name + '.pdf';
+        retrievedFileName = `label_${this.state.bundle.owner.name}.pdf`;
       }
       fileDownload(response.data, retrievedFileName);
-    }
-    catch (error) {
+    } catch (error) {
       console.error(error);
-      notification.addNotification({message: 'download fail', level: 'error'});
+      notification.addNotification({ message: 'download fail', level: 'error' });
     }
   }
 
-  render () {
+  render() {
     return (
       <div>
         <ol className="breadcrumb">
           <li className="breadcrumb-item"><Link to="/admin/manage">Panel d'Administration</Link></li>
           <li className="breadcrumb-item"><Link to="/admin/manage/bundle">Parrainages</Link></li>
-          <li className="breadcrumb-item active">Parrainage{(this.state.bundle)?' de '+this.state.bundle.owner.firstname+' '+this.state.bundle.owner.name:''}</li>
+          <li className="breadcrumb-item active">
+Parrainage
+            {(this.state.bundle) ? ` de ${this.state.bundle.owner.firstname} ${this.state.bundle.owner.name}` : ''}
+          </li>
         </ol>
-        {(this.state.bundle)?
-        <div className="row">
-          <div className="col-lg-6">
-            <Payment
-              state={this.state.bundle.state.toString()}
-              changeState={this.changeState}
-              submitState={this.submitState}
-            />
-            <Bills bundleId={this.state.bundle.id} />
-            <Label
-              labelFilename={this.state.userLabelFilename}
-              downloadLabel={this.downloadLabel}
-              loading={this.state.loading}
-              userId={this.state.bundle.owner.id}
-              mention={this.state.bundle.owner.label_mention}
-              model={this.state.bundle.owner.label_model}
-            />
-          </div>
-          <div className="col-lg-6">
-            <Bundle bundle={this.state.bundle} />
-            {this.state.owner.user_type === 2 ?<Parrains parrain={this.state.owner} bundleLabel={this.state.bundle.label} />
-            :null}
-          </div>
-        </div>
-        :<Loading />}
+        {(this.state.bundle)
+          ? (
+            <div className="row">
+              <div className="col-lg-6">
+                <Price bundleId={this.state.bundle.id} />
+                <Payment
+                  state={this.state.bundle.state.toString()}
+                  changeState={this.changeState}
+                  submitState={this.submitState}
+                />
+                <Bills bundleId={this.state.bundle.id} />
+                <Label
+                  labelFilename={this.state.userLabelFilename}
+                  downloadLabel={this.downloadLabel}
+                  loading={this.state.loading}
+                  userId={this.state.bundle.owner.id}
+                  mention={this.state.bundle.owner.label_mention}
+                  model={this.state.bundle.owner.label_model}
+                />
+              </div>
+              <div className="col-lg-6">
+                <Bundle bundle={this.state.bundle} />
+                {this.state.owner.user_type === 2 ? <Parrains parrain={this.state.owner} bundleLabel={this.state.bundle.label} />
+                  : null}
+              </div>
+            </div>
+          )
+          : <Loading />}
       </div>
-    )
+    );
   }
 });
--- src/components/admin/manage/bundle/tiles/Payment.jsx
+++ src/components/admin/manage/bundle/tiles/Payment.jsx
@@ -1,20 +1,20 @@
-import React from 'react'
+import React from 'react';
 
-const Payment = ( props ) => (
-	<div className="card mb-4 bg-light">
-		<h4 className="card-header">Paiement</h4>
-		<div className="card-body p-2 text-center">
-			<div className="form-group">
-				<select className="form-control" onChange={props.changeState} name="state" value={props.state}>
-					<option value="0">Non reglé</option>
-					<option value="1">Paiement en attente de validation</option>
-					<option value="2">Payé</option>
-					<option value="3">Payé et en place</option>
-				</select>
-			</div>
-			<button className="btn btn-info btn-sm" onClick={props.submitState}>Enregistrer</button>
-		</div>
-	</div>
-)
+const Payment = ({ changeState, submitState, state }) => (
+  <div className="card mb-4 bg-light">
+    <h4 className="card-header">Paiement</h4>
+    <div className="card-body p-2 text-center">
+      <div className="form-group">
+        <select className="form-control" onChange={changeState} name="state" value={state}>
+          <option value="0">Non reglé</option>
+          <option value="1">Paiement en attente de validation</option>
+          <option value="2">Payé</option>
+          <option value="3">Payé et en place</option>
+        </select>
+      </div>
+      <button type="button" className="btn btn-info btn-sm" onClick={submitState}>Enregistrer</button>
+    </div>
+  </div>
+);
 
 export default Payment;
