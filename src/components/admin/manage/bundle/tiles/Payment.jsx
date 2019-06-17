4c13204bc0b9e06eaa1fcfae5b51ade3c8c4e436
c-champouillon <cchampouillon.externe@m6.fr>
2019-06-17 21:00:05 +0200

    Fix layout

--- src/components/admin/manage/bundle/tiles/Payment.jsx
+++ src/components/admin/manage/bundle/tiles/Payment.jsx
@@ -5,15 +5,13 @@ import { Button } from '../../../../utils/Button';
 const Payment = ({ changeState, submitState, state }) => (
   <div className="newcard">
     <h4>Paiement</h4>
-    <div className="card-body p-2 text-center">
-      <select onChange={changeState} name="state" value={state}>
-        <option value="0">Non reglé</option>
-        <option value="1">Paiement en attente de validation</option>
-        <option value="2">Payé</option>
-        <option value="3">Payé et en place</option>
-      </select>
-      <Button type="button" onClick={submitState}>Enregistrer</Button>
-    </div>
+    <select onChange={changeState} name="state" value={state}>
+      <option value="0">Non reglé</option>
+      <option value="1">Paiement en attente de validation</option>
+      <option value="2">Payé</option>
+      <option value="3">Payé et en place</option>
+    </select>
+    <Button type="button" onClick={submitState}>Enregistrer</Button>
   </div>
 );
 
