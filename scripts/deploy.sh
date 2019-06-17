540e3fb7ec53fd77ee6f4439a2ff565f5535c5cf
c-champouillon <cchampouillon.externe@m6.fr>
2019-06-17 21:15:54 +0200

    updated release script

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -3,4 +3,4 @@ curl -X POST -H 'Content-type: application/json' --data '{"text":":warning: Depl
 rsync -r -z -v -u -e "ssh -p 1023" ./build/. dev@82.165.69.217:/srv/confidences-app/build/.
 ssh dev@82.165.69.217 -p 1023 "/home/dev/.nvm/versions/node/v10.10.0/bin/pm2 reload app"
 duration=$(( SECONDS - start ))
-curl -X POST -H 'Content-type: application/json' --data '{"text":":recycle: Release complete !"}' https://hooks.slack.com/services/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
+curl -X POST -H 'Content-type: application/json' --data '{"text":":recycle: Release complete ! '$npm_package_version'"}' https://hooks.slack.com/services/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
