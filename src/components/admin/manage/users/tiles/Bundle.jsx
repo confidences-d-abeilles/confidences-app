76f6461dc155d350db77025a3c023bd2630f41c1
c-champouillon <cchampouillon.externe@m6.fr>
2019-06-17 21:42:22 +0200

    Fix

--- src/components/admin/manage/users/tiles/Bundle.jsx
+++ src/components/admin/manage/users/tiles/Bundle.jsx
@@ -8,25 +8,29 @@ const Bundle = ({ bundle }) => {
     let beginDate = 'N/A';
     let endDate = 'N/A';
     if (typeof bundle.start_date !== 'undefined' && bundle.start_date) {
-      beginDate = moment(bundle.start_date).format("DD/MM/YYYY à HH[h]mm");
+      beginDate = moment(bundle.start_date).format('DD/MM/YYYY à HH[h]mm');
     }
     if (typeof bundle.end_date !== 'undefined' && bundle.end_date) {
-      endDate = moment(bundle.end_date).format("DD/MM/YYYY à HH[h]mm");
+      endDate = moment(bundle.end_date).format('DD/MM/YYYY à HH[h]mm');
     }
 
-    return (<div>
-      <p className="newcard">
-        Parrainage de {bundle.hives ? `${bundle.hives} ruches ` : `${bundle.bees} abeilles `}
-        <State level={bundle.state} />
-        <br />
-        {`Prise d'effet le ${beginDate}`}
-        <br />
-        {`Expiration le ${endDate}`}
-      </p>
-    </div>)
-  } else {
-    return (<p className="text-muted">Cet utilisateur n'a pas encore choisi de parrainage</p>)
+    return (
+      <div>
+        <p className="newcard">
+          {`Parrainage de ${bundle.hives ? `${bundle.hives} ruches ` : `${bundle.bees} abeilles `}`}
+          <State level={bundle.state} />
+          <br />
+          {`Prise d'effet le ${beginDate}`}
+          <br />
+          {`Expiration le ${endDate}`}
+          <br />
+        Options chosies :&nbsp;
+          {bundle.options && bundle.options.map(({ designation }) => <h6>{designation}</h6>)}
+        </p>
+      </div>
+    );
   }
-}
+  return (<p className="text-muted">Cet utilisateur n'a pas encore choisi de parrainage</p>);
+};
 
 export default Bundle;
