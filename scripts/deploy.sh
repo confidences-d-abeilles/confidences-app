cda7ca648ce886fc48dbf48e9fcec15e7d109de8
cchampou <clement@champouillon.com>
2019-06-24 20:22:31 +0200

    Fix deploy script

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,6 +1,6 @@
 start=$SECONDS
 curl -X POST -H 'Content-type: application/json' --data '{"text":":warning: Deploying new release"}' https://hooks.slack.com/services/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
-rsync -r -z -v -u -e "ssh -p 1023" ./build/. dev@82.165.69.217:/srv/confidences-app/build/.
-ssh dev@82.165.69.217 -p 1023 "/home/dev/.nvm/versions/node/v10.10.0/bin/pm2 reload app"
+rsync -r -z -v -u ./build/. root@212.227.198.124:/srv/confidences-app/build/.
+ssh root@212.227.198.124 "/home/dev/.nvm/versions/node/v10.10.0/bin/pm2 reload app"
 duration=$(( SECONDS - start ))
 curl -X POST -H 'Content-type: application/json' --data '{"text":":recycle: Release complete ! '$npm_package_version'"}' https://hooks.slack.com/services/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
