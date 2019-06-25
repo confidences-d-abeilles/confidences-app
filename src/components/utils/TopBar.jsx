5eb2c4fbef481b443cd700369c04cfcc72c2d73d
cchampou <clement@champouillon.com>
2019-06-25 19:18:44 +0200

    topbar

---
+++ src/components/utils/TopBar.jsx
@@ -0,0 +1,12 @@
+import styled from '@emotion/styled';
+
+const TopBar = styled('div')`
+  width: 100%;
+  background-color: black;
+  padding: 0.5rem;
+  text-align: center;
+  color: silver;
+  font-size: 0.9rem;
+`;
+
+export default TopBar;
