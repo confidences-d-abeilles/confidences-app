2dff931d97678a5f70c4f8069f31c3a65b33565c
cchampou <clement@champouillon.com>
2019-07-29 23:02:47 +0200

    More on eslint

--- src/components/utils/FileUpload.jsx
+++ src/components/utils/FileUpload.jsx
@@ -13,21 +13,34 @@ export default class FileUpload extends Component {
     newFile: null,
   };
 
-  identifier = (this.props.identifier)?this.props.identifier:Math.floor(Math.random() * 1000);
+  // eslint-disable-next-line react/destructuring-assignment
+  identifier = (this.props.identifier) ? this.props.identifier : Math.floor(Math.random() * 1000);
 
   render() {
+    const { loading, label } = this.props;
     return (
       <div className="form-group">
-        <label>{this.props.label}</label>
-        {(this.props.loading)?
-          <Loading />
-          :
-        <label htmlFor="logo" className={(this.state.newFile)?'active-upload':'upload'} style={{ position: 'relative' }}>
-          <input type="file" className="form-control" id={this.identifier} onChange={() => { this.setState({ newFile : document.getElementById(this.identifier).files[0].name }) }} style={{ position: 'absolute', height: '5.5em', top: '0', left: "0", opacity: '0.0001'}} accept={(this.props.accept)?this.props.accept:''} />
-          Glissez un fichier ici ou cliquez pour en sélectionner un<br/>
-          {(this.state.newFile)?'Sélectionné : '+this.state.newFile:"Aucun fichier sélectionné"}
-        </label>}
+        <label>{label}</label>
+        {loading
+          ? <Loading />
+          : (
+            <label htmlFor="logo" className={(this.state.newFile) ? 'active-upload' : 'upload'} style={{ position: 'relative' }}>
+              <input
+                type="file"
+                className="form-control"
+                id={this.identifier}
+                onChange={() => { this.setState({ newFile: document.getElementById(this.identifier).files[0].name }); }}
+                style={{
+                  position: 'absolute', height: '5.5em', top: '0', left: '0', opacity: '0.0001',
+                }}
+                accept={(this.props.accept) ? this.props.accept : ''}
+              />
+          Glissez un fichier ici ou cliquez pour en sélectionner un
+              <br />
+              {(this.state.newFile) ? `Sélectionné : ${this.state.newFile}` : 'Aucun fichier sélectionné'}
+            </label>
+          )}
       </div>
-    )
+    );
   }
 }
