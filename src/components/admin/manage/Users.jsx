a8f96bf8a1414211b0bed750bd7983617cdf628f
cchampou <clement@champouillon.com>
2019-07-30 20:45:25 +0200

    Lint

--- src/components/admin/manage/Users.jsx
+++ src/components/admin/manage/Users.jsx
@@ -8,24 +8,24 @@ import request from '../../../services/Net';
 import Meta from '../../utils/Meta';
 import { withNotification } from '../../../services/withNotification';
 
-export default withNotification(class MainScreen extends Component {
+class MainScreen extends Component {
   state = {
-    users : [],
-    filtered : [],
-    selectedId : null,
-    filters :{
-      e : true,
-      p : true,
-      aa : true,
-      edit : true,
-      admin : true,
-      unpaid : true,
-      waiting : true,
-      paid : true,
-      done : true,
-      no : true
+    users: [],
+    filtered: [],
+    selectedId: null,
+    filters: {
+      e: true,
+      p: true,
+      aa: true,
+      edit: true,
+      admin: true,
+      unpaid: true,
+      waiting: true,
+      paid: true,
+      done: true,
+      no: true,
     },
-    criteria : ''
+    criteria: '',
   };
 
   componentDidMount() {
@@ -37,34 +37,35 @@ export default withNotification(class MainScreen extends Component {
     const { notification } = this.props;
     request({
       url: '/user',
-      method: 'get'
+      method: 'get',
     }, notification).then((res) => {
       this.setState({
-        users : res
+        users: res,
       }, () => {
         this.filter();
       });
-    })
+    });
   }
 
   checkFilter = (e) => {
     this.setState({
-      filters : {
+      filters: {
         ...this.state.filters,
-        [e.target.name] : !this.state.filters[e.target.name]
-      }
-    }, () => { this.filter() });
+        [e.target.name]: !this.state.filters[e.target.name],
+      },
+    }, () => { this.filter(); });
   }
 
   search = (e) => {
     this.setState({
-      criteria : e.target.value
+      criteria: e.target.value,
     }, () => {
-      this.filter()
-    })
+      this.filter();
+    });
   }
+
   filter = () => {
-    let tmp = this.state.users.filter((e) => (
+    let tmp = this.state.users.filter(e => (
       ((e.user_type === 1 && this.state.filters.p)
       || (e.user_type === 2 && this.state.filters.e)
       || (e.user_type === 3 && this.state.filters.aa)
@@ -80,45 +81,44 @@ export default withNotification(class MainScreen extends Component {
       if ((e.firstname.toLowerCase().indexOf(this.state.criteria.toLowerCase()) >= 0)
       || (e.name.toLowerCase().indexOf(this.state.criteria.toLowerCase()) >= 0)
       || (e.company_name.toLowerCase().indexOf(this.state.criteria.toLowerCase()) >= 0) || this.state.criteria === '') {
-        return true
-      } else {
-        return false;
+        return true;
       }
-    })
+      return false;
+    });
     this.setState({
-      filtered : tmp
-    })
+      filtered: tmp,
+    });
   }
 
   select = (id) => {
     this.setState({
-      selectedId : id
-    })
+      selectedId: id,
+    });
   }
 
   checkValidation = (e) => {
     if (e.key === 'Enter') {
-      this.setState({ selectedId : this.state.filtered[0].id })
+      this.setState({ selectedId: this.state.filtered[0].id });
     }
     if (e.key === 'ArrowDown') {
       const tmp = this.state.filtered;
       const row = tmp.shift();
       tmp.push(row);
       this.setState({
-        filtered : tmp
-      })
+        filtered: tmp,
+      });
     }
     if (e.key === 'ArrowUp') {
       const tmp = this.state.filtered;
       const row = tmp.pop();
       tmp.unshift(row);
       this.setState({
-        filtered : tmp
-      })
+        filtered: tmp,
+      });
     }
   }
 
-  render () {
+  render() {
     const csvData = this.state.filtered.map(({
       firstname,
       name,
@@ -140,8 +140,8 @@ export default withNotification(class MainScreen extends Component {
     }));
     return (
       <div>
-        <Meta title="Gérer les utilisateurs"/>
-        {this.state.selectedId && <Redirect to={'/admin/manage/user/'+this.state.selectedId} push />}
+        <Meta title="Gérer les utilisateurs" />
+        {this.state.selectedId && <Redirect to={`/admin/manage/user/${this.state.selectedId}`} push />}
         <div className="row">
           <div className="col">
             <ol className="breadcrumb">
@@ -157,24 +157,76 @@ export default withNotification(class MainScreen extends Component {
             <CSVLink data={csvData}>Exporter</CSVLink>
           </div>
           <div className="col">
-            <label htmlFor="p"><input type="checkbox" name="p" id="p" checked={this.state.filters.p} onChange={this.checkFilter} /> Particuliers</label><br />
-            <label htmlFor="e"><input type="checkbox" name="e" id="e" checked={this.state.filters.e} onChange={this.checkFilter} /> Entreprises</label><br />
-            <label htmlFor="aa"><input type="checkbox" name="aa" id="aa" checked={this.state.filters.aa} onChange={this.checkFilter} /> Apporteurs d'Affaires</label><br />
-            <label htmlFor="edit"><input type="checkbox" name="edit" id="edit" checked={this.state.filters.edit} onChange={this.checkFilter} /> Editeurs</label><br />
-            <label htmlFor="admin"><input type="checkbox" name="admin" id="admin" checked={this.state.filters.admin} onChange={this.checkFilter} /> Admins</label><br />
+            <label htmlFor="p">
+              <input type="checkbox" name="p" id="p" checked={this.state.filters.p} onChange={this.checkFilter} />
+              {' '}
+Particuliers
+            </label>
+            <br />
+            <label htmlFor="e">
+              <input type="checkbox" name="e" id="e" checked={this.state.filters.e} onChange={this.checkFilter} />
+              {' '}
+Entreprises
+            </label>
+            <br />
+            <label htmlFor="aa">
+              <input type="checkbox" name="aa" id="aa" checked={this.state.filters.aa} onChange={this.checkFilter} />
+              {' '}
+Apporteurs d'Affaires
+            </label>
+            <br />
+            <label htmlFor="edit">
+              <input type="checkbox" name="edit" id="edit" checked={this.state.filters.edit} onChange={this.checkFilter} />
+              {' '}
+Editeurs
+            </label>
+            <br />
+            <label htmlFor="admin">
+              <input type="checkbox" name="admin" id="admin" checked={this.state.filters.admin} onChange={this.checkFilter} />
+              {' '}
+Admins
+            </label>
+            <br />
           </div>
           <div className="col">
-            <label htmlFor="unpaid"><input type="checkbox" name="unpaid" id="unpaid" checked={this.state.filters.unpaid} onChange={this.checkFilter} /> Non réglé</label><br />
-            <label htmlFor="waiting"><input type="checkbox" name="waiting" id="waiting" checked={this.state.filters.waiting} onChange={this.checkFilter} /> En attente</label><br />
-            <label htmlFor="paid"><input type="checkbox" name="paid" id="paid" checked={this.state.filters.paid} onChange={this.checkFilter} /> Payé</label><br />
-            <label htmlFor="done"><input type="checkbox" name="done" id="done" checked={this.state.filters.done} onChange={this.checkFilter} /> En place</label><br />
-            <label htmlFor="no"><input type="checkbox" name="no" id="no" checked={this.state.filters.no} onChange={this.checkFilter} /> Pas de parraiange</label><br />
+            <label htmlFor="unpaid">
+              <input type="checkbox" name="unpaid" id="unpaid" checked={this.state.filters.unpaid} onChange={this.checkFilter} />
+              {' '}
+Non réglé
+            </label>
+            <br />
+            <label htmlFor="waiting">
+              <input type="checkbox" name="waiting" id="waiting" checked={this.state.filters.waiting} onChange={this.checkFilter} />
+              {' '}
+En attente
+            </label>
+            <br />
+            <label htmlFor="paid">
+              <input type="checkbox" name="paid" id="paid" checked={this.state.filters.paid} onChange={this.checkFilter} />
+              {' '}
+Payé
+            </label>
+            <br />
+            <label htmlFor="done">
+              <input type="checkbox" name="done" id="done" checked={this.state.filters.done} onChange={this.checkFilter} />
+              {' '}
+En place
+            </label>
+            <br />
+            <label htmlFor="no">
+              <input type="checkbox" name="no" id="no" checked={this.state.filters.no} onChange={this.checkFilter} />
+              {' '}
+Pas de parraiange
+            </label>
+            <br />
           </div>
         </div>
         <div className="row">
           <List data={this.state.filtered} select={this.select} />
         </div>
       </div>
-    )
+    );
   }
-});
+};
+
+export default withNotification(MainScreen);
--- src/components/admin/manage/users/List.jsx
+++ src/components/admin/manage/users/List.jsx
@@ -1,31 +1,33 @@
-import React from 'react'
-import Loading from '../../../utils/Loading'
-import State from './State/State'
-import UserType from './UserType/UserType'
+import React from 'react';
+import Loading from '../../../utils/Loading';
+import State from './State/State';
+import UserType from './UserType/UserType';
 
-const List = (props) => (
-  (props.data) ?
-    <table className="table table-hover">
-      <thead>
-        <tr>
-          <th><input type="checkbox" /></th>
-          <th>Type d'utilisateur</th>
-          <th>Nom de l'utilisateur</th>
-          <th>État du parrainage</th>
-        </tr>
-      </thead>
-      <tbody>
-        {props.data.map((user) => (
-          <tr key={user.id} onClick={props.select.bind(this, user.id)} style={{ cursor: 'pointer' }} >
-            <td><input type="checkbox" /></td>
-            <td><UserType type={user.user_type} /></td>
-            <td>{user.firstname + ' ' + user.name + ' ' + user.company_name}</td>
-            <td>{(user.bundles[0]) ? <State level={user.bundles[0].state} /> : 'Pas de parrainage'}</td>
+const List = ({ data, select }) => (
+  data
+    ? (
+      <table className="table table-hover">
+        <thead>
+          <tr>
+            <th><input type="checkbox" /></th>
+            <th>Type d'utilisateur</th>
+            <th>Nom de l'utilisateur</th>
+            <th>État du parrainage</th>
           </tr>
-        ))}
-      </tbody>
-    </table>
+        </thead>
+        <tbody>
+          {data.map(user => (
+            <tr key={user.id} onClick={select.bind(this, user.id)} style={{ cursor: 'pointer' }}>
+              <td><input type="checkbox" /></td>
+              <td><UserType type={user.user_type} /></td>
+              <td>{`${user.firstname} ${user.name} ${user.company_name}`}</td>
+              <td>{(user.bundles[0]) ? <State level={user.bundles[0].state} /> : 'Pas de parrainage'}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    )
     : <Loading />
-)
+);
 
 export default List;
