f282bb68fda7204cd1a20f2920d2b3f008258895
Clément Champouillon <clement@champouillon.com>
2019-08-27 18:10:21 +0200

    chore(docker): Init ⚓️ (#241)

--- server.js
+++ server.js
@@ -1,16 +1,17 @@
 require('dotenv').config({
-    path: process.env.NODE_ENV ? `.env.${process.env.NODE_ENV}` : '.env.production',
+  path: process.env.NODE_ENV ? `.env.${process.env.NODE_ENV}` : '.env.production',
 });
 const express = require('express');
-const app = express();
-const path = require('path');
-const fs = require('fs');
 
-const metaResolverMiddleware = require('./server/metaResolver').middleware;
+const app = express();
+const fs = require('fs');
 
 const compression = require('compression');
 
-const html = fs.readFileSync('./build/index.html');
+const metaResolverMiddleware = require('./server/metaResolver').middleware;
+
+
+const htmlContent = fs.readFileSync('./build/index.html');
 
 const metaLoader = require('./public/meta/meta');
 
@@ -22,38 +23,35 @@ app.use('/static', express.static('build/static'));
 
 app.use('/', metaResolverMiddleware);
 
+function composeHtml(html, meta) {
+  const splitted = html.split('<title>Confidences d\'Abeilles</title>');
+  let output = '';
+  if (meta.title) {
+    output = `${output}<title>${meta.title}</title>`;
+  }
+  if (meta.ogtitle) {
+    output = `${output}<meta id="og-title" property="og:title" content="${meta.ogtitle}" />`;
+  }
+
+
+  if (meta.ogdescription) {
+    output = `${output}<meta id="og-description" property="og:description" content="${meta.ogdescription}" />`;
+    output = `${output}<meta name="description" content="${meta.ogdescription}" />`;
+  }
+  if (meta.ogurl) {
+    output = `${output}<meta id="og-url" property="og:url" content="${meta.ogurl}" />`;
+  }
+  if (meta.ogimg) {
+    output = `${output}<meta id="og-image" property="og:image" content="${meta.ogimg}" />`;
+  }
+  return splitted[0] + output + splitted[2];
+}
+
 app.get('/*', (req, res) => {
-    res.end(composeHtml(html.toString(), req.meta ? req.meta : metaLoader.load(req.url)));
+  res.end(composeHtml(htmlContent.toString(), req.meta ? req.meta : metaLoader.load(req.url)));
 });
 
 app.listen(process.env.PORT || 5000, () => {
-    console.log("Server started");
+  // eslint-disable-next-line no-console
+  console.log('Server started');
 });
-
-
-function composeHtml(html, meta) {
-
-    let splitted = html.split('<title>Confidences d\'Abeilles</title>');
-    let output = "";
-    if (meta.title) {
-        output = output + '<title>' + meta.title + '</title>';
-    }
-    if (meta.ogtitle) {
-        output = output + '<meta id="og-title" property="og:title" content="' + meta.ogtitle + '" />';
-    }
-
-
-    if (meta.ogdescription) {
-      output = `${output}<meta id="og-description" property="og:description" content="${meta.ogdescription}" />`;
-      output = `${output}<meta name="description" content="${meta.ogdescription}" />`;
-    }
-    if (meta.ogurl) {
-        output = output + '<meta id="og-url" property="og:url" content="'+meta.ogurl+'" />';
-    }
-    if (meta.ogimg) {
-        output = output + '<meta id="og-image" property="og:image" content="' + meta.ogimg + '" />';
-    }
-    return splitted[0] + output + splitted[2];
-}
-
-
