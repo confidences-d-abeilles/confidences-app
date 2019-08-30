9c674373d43284b6866394ec3e6526256da86ef2
cchampou <clement@champouillon.com>
2019-08-30 22:05:02 +0200

    fix(redux): remove devtools :construction:

--- src/App.jsx
+++ src/App.jsx
@@ -28,8 +28,7 @@ const ScrollToTop = () => {
 
 const store = createStore(
   reducers,
-  compose(applyMiddleware(thunk),
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()),
+  compose(applyMiddleware(thunk)),
 );
 
 const App = () => (
