c84ba5b83d836b8aa6746d318eb520227b3dd003
cchampou <clement@champouillon.com>
2019-08-31 00:24:56 +0200

    Namespaced actions

--- src/modules/adminManageHives/hives.actions.js
+++ src/modules/adminManageHives/hives.actions.js
@@ -1,11 +1,11 @@
-export const HIVES_FETCH = 'HIVES_FETCH';
-export const HIVES_FETCH_SUCCESS = 'HIVES_FETCH_SUCCESS';
-export const HIVES_FETCH_FAIL = 'HIVES_FETCH_FAIL';
+export const HIVES_FETCH = 'admin/manage/hives/FETCH';
+export const HIVES_FETCH_SUCCESS = 'admin/manage/hives/FETCH_SUCCESS';
+export const HIVES_FETCH_FAIL = 'admin/manage/hives/FETCH_FAIL';
 
 export const fetchHives = needle => ({ type: HIVES_FETCH, needle });
 
-export const HIVES_ADD = 'HIVES_ADD';
-export const HIVES_ADD_SUCCESS = 'HIVES_ADD_SUCCESS';
-export const HIVES_ADD_FAIL = 'HIVES_ADD_FAIL';
+export const HIVES_ADD = 'admin/manage/hives/ADD';
+export const HIVES_ADD_SUCCESS = 'admin/manage/hives/ADD_SUCCESS';
+export const HIVES_ADD_FAIL = 'admin/manage/hives/ADD_FAIL';
 
 export const addHive = name => ({ type: HIVES_ADD, name });
