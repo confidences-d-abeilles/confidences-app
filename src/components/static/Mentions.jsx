5b43f7a39ce248522f5b8ad918fc243cd9f1f9cd
cchampou <clement@champouillon.com>
2019-10-03 19:52:08 +0200

    Mentions legales

--- src/components/static/Mentions.jsx
+++ src/components/static/Mentions.jsx
@@ -6,43 +6,130 @@ export default () => (
   <div className="container py-4">
     <Meta title="Mentions légales" />
     <h2>Mentions légales</h2>
-    <h3>Editeur du site et prestataire de service :</h3>
-    SAS Confidences d’Abeilles
-    <br />
-    Société par actions simplifiée au capital de 3 000,00 euros
-    <br />
-    Siège social situé au 319 chemin des plantées, 74 210 Faverges
-    <br />
-    RCS Annecy 814 152 518
-    <br />
-    <strong>SIRET : </strong>
-    814 152 518 000 19
-    <br />
-    <strong>Numéro de téléphone : 07 67 37 41 44</strong>
-    <br />
-    <strong>Email : </strong>
-    general@parrainagederuches.fr
-    <br />
-    <strong>Directeur de la publication : </strong>
-    Gaëtan EKSZTEROWICZ
-    <br />
-    <br />
-
-    <strong>Déclaration CNIL :</strong>
-    <br />
-    Conformément à la loi &quot; Informatique et Libertés &quot;, le traitement des informations nominatives
-    relatives aux clients à fait l&#39;objet d&#39;une déclaration auprès de la Commission Nationale de
-    l&#39;Informatique et des Libertés (CNIL) sous le numéro 1943859
-    <br />
-    <br />
-    <strong>Hébergement du présent site</strong>
-    <br />
-    Le prestataire assurant le stockage direct et permanent du site est la société 1&1 Internet SARL
-    <br />
-    7, place de la Gare – BP 70109 – 57201 Sarreguemines Cedex
-    <br />
-    Numéro de téléphone : 0970 808 911
-    <br />
-
+    <p>L’Association Confidences d’Abeilles est une association loi 1901, reconnue d’intérêt général.</p>
+    <h3>Editeur</h3>
+    <p>
+      Association Confidences d’Abeilles
+      <br />
+      <strong>Siège social : </strong>
+      319 chemin des plantées, 74 210 Faverges
+      <br />
+      <strong>SIRET : </strong>
+      853 866 275 00011
+      <br />
+      <strong>Numéro de téléphone : </strong>
+      <br />
+      <strong>Site : </strong>
+      association.confidencesdabeilles.fr
+      <br />
+      <strong>Email : </strong>
+      asso[at]confidencesdabeilles(point)fr
+    </p>
+    <h3>Réalisation</h3>
+    <p>
+      Agence 3BEE
+      <br />
+      4 rue lafayette, 38 000 Grenoble
+    </p>
+    <h3>Données personnelles</h3>
+    <p>
+      L’Association Confidences d’Abeilles assure la protection, la confidentialité, et la sécurité des
+      données à caractère personnel que vous lui confiez, conformément à la législation en vigueur.
+      <br />
+      Elle s’engage aussi à respecter, en tant que responsable de traitement, les dispositions de la Loi n°78-
+      17 du 6 janvier 1978 modifiée et le Règlement général sur la protection des données (UE 2016/679)
+      paru au journal officiel le 27 avril 2016 et en vigueur depuis le 25 mai 2018.
+      <br />
+      Elle se dégage de toute responsabilité concernant :
+    </p>
+    <ul>
+      <li>
+tous dommages causés suite à une intrusion frauduleuse d'un tiers sur son site ayant
+          provoqué une modification des informations présentes.
+      </li>
+      <li>
+tous dommages, directs ou indirects, sans aucune distinction des causes, origines, natures ou
+          conséquences, provoqués suite à l'accès d’une personne non autorisée sur son site, ainsi que
+          l'utilisation des informations qu'il contient.
+      </li>
+    </ul>
+    <h3>Données collectées</h3>
+    <p>
+      L’Association Confidences d’Abeilles minimise la collecte et le traitement de données en ne
+      s’intéressant qu’à celles nécessaires à son bon fonctionnement et à l’atteinte d’objectifs
+      opérationnels. Elle prend également toutes les dispositions utiles afin que les données soient
+      exactes, complètes et, si nécessaire, mises à jour.
+    </p>
+    <h3>Fonctionnement et objectifs opérationnels</h3>
+    <ul>
+      <li>
+La gestion des relations avec ses membres, ses donateurs, ses utilisateurs, et ses
+        partenaires : réponse aux demandes, édition des reçus fiscaux, envoi d’information,
+        campagne de dons.
+      </li>
+      <li>
+La gestion la communication externe (newsletters, rapports d’activité, documents
+        pédagogiques).
+      </li>
+      <li>Statistiques et mesure de fréquentation du site.</li>
+    </ul>
+    <h3>Destination des données</h3>
+    <p>
+      L’ensemble des données collectées est utilisé en interne par l’Association Confidences d’Abeilles et
+      elle ne communique que le strict nécessaire à ses prestataires de services (service postal par
+      exemple). Ces prestataires sont tenus par une obligation de confidentialité.
+    </p>
+    <h3>Conservation des données</h3>
+    <p>
+      L’Association Confidences d’Abeilles ne conserve les données que pour la durée nécessaire à la
+      réalisation des objectifs opérationnels cités ci-dessus et pour lesquelles elles ont été collectées. Cette
+      durée est variable selon les objectifs opérationnels.
+      <br />
+      Pour la Newsletter, les données personnelles sont conservées sans limitation de durée après
+      l’inscription. Il est possible de se désinscrire en suivant les liens de désinscription sur chaque
+      newsletter ou directement depuis son compte utilisateur.
+    </p>
+    <h3>Sécurité des données</h3>
+    <p>
+      Les données à caractère personnel sont stockées en Allemagne dans les bases de données mises en
+      place pour l’Association Confidences d’Abeilles. L’agence 3BEE et l’hébergeur mettent en place les
+      moyens techniques appropriés pour éviter l’altération ou la perte des données ainsi que tout accès
+      non autorisé à celles-ci.
+    </p>
+    <h3>Cookies</h3>
+    <p>
+      Les cookies sont des identifiants uniques, enregistrés par le serveur du site Internet sur votre
+      appareil afin de permettre à notre système de le reconnaître mais ils ne permettent pas d’identifier
+      une personne physique. Nous utilisons des cookies pour fluidifier votre navigation sur le site.
+    </p>
+    <h3>Vos droits</h3>
+    <p>
+      Conformément à la Loi « Informatique et Libertés » modifiée, vous disposez d’un droit d’accès, de
+      rectification et d’effacement de vos données à caractère personnel, du droit de limiter ou de vous
+      opposer à leur traitement ainsi que d’un droit à leur portabilité. Vous pouvez également introduire
+      une réclamation auprès de la CNIL.
+      <br />
+      <strong>Entreprises mécènes : </strong>
+      Sauf opposition de votre part, l’Association Confidences d’Abeilles est
+      susceptible d’utiliser votre nom, les éléments visuels communiqués (logo par exemple), ou les photos
+      des ruches de votre entreprise dans sa communication externe, dans la presse, sur son propre site
+      internet, et sur ses réseaux sociaux.
+      <br />
+      Vos droits s’exercent auprès de l’Association Confidences d’Abeilles par mail à l’adresse suivante
+      asso[at]confidencesdabeilles(point)fr.
+      <br />
+      Si vous ne souhaitez plus recevoir les Newsletters de l’Association Confidences d’Abeilles, vous
+      pouvez vous désinscrire en cliquant sur le lien situé en bas de chaque communication ou bien
+      demander la désinscription depuis votre compte utilisateur.
+    </p>
+    <h3>Droits d’auteur</h3>
+    <p>
+      L’ensemble de ce site relève de la législation française et internationale sur le droit d’auteur et la
+      propriété intellectuelle. Tous les droits de reproduction sont réservés, y compris pour les documents
+      téléchargeables, les photos, les schémas, les logos. La reproduction de tout ou partie de ce site est
+      interdite sauf avec notre autorisation expresse.
+    </p>
+    <h3>Crédits photos</h3>
+    <p>Hélène Blin / Gaëtan Ekszterowicz / Tristan Kistler / Mickaël Philipot / Léa Sengele</p>
   </div>
 );
