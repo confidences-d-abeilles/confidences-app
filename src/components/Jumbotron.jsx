55c86c481a8c8384de7a48231069966a2078a2cb
Clément Champouillon <clement@champouillon.com>
2019-11-14 22:00:40 +0100

    feat(slider): jumbtron can now handle multiple images (#275)

--- src/components/Jumbotron.jsx
+++ src/components/Jumbotron.jsx
@@ -1,5 +1,5 @@
 import styled from '@emotion/styled';
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import PropTypes from 'prop-types';
 import { Item, Rows } from '@cda/flex';
 
@@ -37,25 +37,74 @@ const JumboImage = styled(Item)`
   width: 100%;
   align-self: stretch;
   flex: 2;
-  background-image: ${props => `url("${props.src}")`};
-  background-size: cover;
-  background-position: center;
+  background-color: black;
+  position: relative;
+  
   @media(max-width: 800px) {
     display: none;
   }
 `;
 
-const Jumbotron = ({ children, img }) => (
-  <JumbotronWrapper alignItems="center">
-    <Jumbcontent flex={1}>
-      {children}
-    </Jumbcontent>
-    <JumboImage src={img} flex={1} />
-  </JumbotronWrapper>
-);
+const Image = styled('img')`
+  opacity: ${({ show }) => (show ? 1 : 0)};
+  width: 100%;
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  transition: opacity 1s ease-in-out;
+`;
+
+const Indicator = styled(Rows)`
+  z-index: 10;
+  position: absolute;
+  left: 50%;
+  bottom: 1rem;
+  background-color: rgba(0, 0, 0, 0.25);
+  border-radius: 2rem;
+`;
+
+const IndicatorDot = styled(Item)`
+  width: 0.75rem;
+  height: 0.75rem;
+  background-color: white;
+  opacity: ${({ focus }) => (focus ? 0.9 : 0.2)};
+  box-shadow: 0 0 5px gray;
+  margin: 0.5rem;
+  cursor: pointer;
+  border-radius: 50%;
+  transition: opacity 1s ease-in-out;
+`;
+
+const Jumbotron = ({ children, img }) => {
+  const [currentIndex, setCurrentIndex] = useState(0);
+  useEffect(() => {
+    const ticker = setInterval(() => {
+      setCurrentIndex(currentIndex + 1 === img.length ? 0 : currentIndex + 1);
+    }, 4000);
+    return () => clearInterval(ticker);
+  }, [currentIndex]);
+
+  return (
+    <JumbotronWrapper alignItems="center">
+      <Jumbcontent flex={1}>
+        {children}
+      </Jumbcontent>
+      <JumboImage flex={1}>
+        {img.map((currentImg, index) => <Image src={currentImg} show={index === currentIndex} />)}
+        {img.length > 1 && (
+        <Indicator>
+          {img.map((_, index) =>
+            <IndicatorDot focus={index === currentIndex} onClick={() => setCurrentIndex(index)} />)}
+        </Indicator>
+        )}
+      </JumboImage>
+    </JumbotronWrapper>
+  );
+};
 
 Jumbotron.propTypes = {
-  img: PropTypes.string.isRequired,
+  img: PropTypes.arrayOf(PropTypes.string.isRequired).isRequired,
   children: PropTypes.node.isRequired,
 };
 
